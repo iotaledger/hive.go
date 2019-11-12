a71ef13e13007cf8cdd5035141d817c2bae6945c
Luca Moser <moser.luca@gmail.com>
2019-11-12 17:15:25 +0100

    adds lru cache, doubly linked list, key mutex

---
+++ sync/krw_mutex_test.go
@@ -0,0 +1,24 @@
+package key_mutex_test
+
+import (
+	key_mutex "github.com/iotaledger/hive.go/sync"
+	"testing"
+)
+
+func TestKRWMutex_Free(t *testing.T) {
+	krwMutex := key_mutex.NewKRWMutex()
+
+	krwMutex.Register("test")
+	krwMutex.Register("test")
+	krwMutex.Free("test")
+	krwMutex.Free("test")
+}
+
+func BenchmarkKRWMutex(b *testing.B) {
+	krwMutex := key_mutex.NewKRWMutex()
+
+	for i := 0; i < b.N; i++ {
+		krwMutex.Register(i)
+		krwMutex.Free(i)
+	}
+}
