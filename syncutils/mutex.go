f0afc843bee56644e0933c19bebefc50e97776fd
Luca Moser <moser.luca@gmail.com>
2019-11-13 17:02:06 +0100

    use type aliases for mutexes

--- syncutils/mutex.go
+++ syncutils/mutex.go
@@ -6,13 +6,5 @@ import (
 	"sync"
 )
 
-type Mutex sync.Mutex
-type RWMutex sync.RWMutex
-
-func (m *Mutex) Lock()   { (*sync.Mutex)(m).Lock() }
-func (m *Mutex) Unlock() { (*sync.Mutex)(m).Unlock() }
-
-func (m *RWMutex) Lock()    { (*sync.RWMutex)(m).Lock() }
-func (m *RWMutex) Unlock()  { (*sync.RWMutex)(m).Unlock() }
-func (m *RWMutex) RLock()   { (*sync.RWMutex)(m).RLock() }
-func (m *RWMutex) RUnlock() { (*sync.RWMutex)(m).RUnlock() }
+type Mutex = sync.Mutex
+type RWMutex = sync.RWMutex
--- syncutils/mutex_deadlock.go
+++ syncutils/mutex_deadlock.go
@@ -7,17 +7,9 @@ import (
 	"time"
 )
 
-type Mutex deadlock.Mutex
-type RWMutex deadlock.RWMutex
+type Mutex = deadlock.Mutex
+type RWMutex = deadlock.RWMutex
 
 func init() {
 	deadlock.Opts.DeadlockTimeout = time.Duration(20 * time.Second)
 }
-
-func (m *Mutex) Lock()   { (*deadlock.Mutex)(m).Lock() }
-func (m *Mutex) Unlock() { (*deadlock.Mutex)(m).Unlock() }
-
-func (m *RWMutex) Lock()    { (*deadlock.RWMutex)(m).Lock() }
-func (m *RWMutex) Unlock()  { (*deadlock.RWMutex)(m).Unlock() }
-func (m *RWMutex) RLock()   { (*deadlock.RWMutex)(m).RLock() }
-func (m *RWMutex) RUnlock() { (*deadlock.RWMutex)(m).RUnlock() }
