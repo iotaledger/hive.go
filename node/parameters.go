92f05839b6e080d3fb7154a5e89d85049542055f
Luca Moser <moser.luca@gmail.com>
2019-11-15 14:44:40 +0100

    adds node config keys

--- node/parameters.go
+++ node/parameters.go
@@ -5,8 +5,14 @@ import (
 	flag "github.com/spf13/pflag"
 )
 
+const (
+	CFG_LOG_LEVEL       = "node.LogLevel"
+	CFG_DISABLE_PLUGINS = "node.disablePlugins"
+	CFG_ENABLE_PLUGINS  = "node.enablePlugins"
+)
+
 func init() {
-	flag.Int("node.LogLevel", int(logger.LevelInfo), "controls the log types that are shown")
-	flag.StringSlice("node.DisablePlugins", nil, "a list of plugins that shall be disabled")
-	flag.StringSlice("node.EnablePlugins", nil, "a list of plugins that shall be enabled")
+	flag.Int(CFG_LOG_LEVEL, int(logger.LevelInfo), "controls the log types that are shown")
+	flag.String(CFG_DISABLE_PLUGINS, "", "a list of plugins that shall be disabled")
+	flag.String(CFG_ENABLE_PLUGINS, "", "a list of plugins that shall be enabled")
 }
