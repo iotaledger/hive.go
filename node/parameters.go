8a8861c2801fb727a2991ee20ddc8ca4baba963e
Luca Moser <moser.luca@gmail.com>
2019-11-15 14:26:34 +0100

    remove log levels from node

--- node/parameters.go
+++ node/parameters.go
@@ -1,9 +1,12 @@
 package node
 
-import flag "github.com/spf13/pflag"
+import (
+	"github.com/iotaledger/hive.go/logger"
+	flag "github.com/spf13/pflag"
+)
 
 func init() {
-	flag.Int("node.LogLevel", LOG_LEVEL_INFO, "controls the log types that are shown")
+	flag.Int("node.LogLevel", int(logger.LevelInfo), "controls the log types that are shown")
 	flag.StringSlice("node.DisablePlugins", nil, "a list of plugins that shall be disabled")
 	flag.StringSlice("node.EnablePlugins", nil, "a list of plugins that shall be enabled")
 }
