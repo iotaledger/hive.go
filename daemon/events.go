b80b08da08e4d527b952c76151bfcd331fc90e50
Luca Moser <moser.luca@gmail.com>
2019-12-06 01:14:01 +0100

    adds daemon with shutdown order and keeps old one in place for GoShimmer

--- daemon/events.go
+++ daemon/events.go
@@ -1,13 +1,1 @@
 package daemon
-
-import (
-	"github.com/iotaledger/hive.go/events"
-)
-
-var Events = struct {
-	Run      *events.Event
-	Shutdown *events.Event
-}{
-	Run:      events.NewEvent(events.CallbackCaller),
-	Shutdown: events.NewEvent(events.CallbackCaller),
-}
