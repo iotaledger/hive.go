1fcc3bb36b71878adad48498ecfd4232e6f4013a
Hans Moog <hm@mkjc.net>
2019-12-31 01:50:43 +0100

    Feat: added a new ants based workerpool

---
+++ async/workerpool.go
@@ -0,0 +1,118 @@
+package async
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+
+	"github.com/panjf2000/ants/v2"
+
+	"github.com/iotaledger/hive.go/typeutils"
+)
+
+// Implements a blocking goroutine pool with fixed capacity, managing and recycling a massive number of goroutines,
+// allowing developers to limit the number of goroutines in your concurrent programs.
+type WorkerPool struct {
+	pool         *ants.Pool
+	stopped      typeutils.AtomicBool
+	tasksWg      sync.WaitGroup
+	initOnce     sync.Once
+	shutdownOnce sync.Once
+}
+
+// Submits a task to this pool (it waits if not enough workers are available).
+func (workerPool *WorkerPool) Submit(f func()) {
+	if workerPool.stopped.IsSet() {
+		return
+	}
+
+	workerPool.tasksWg.Add(1)
+
+	if antsErr := workerPool.get().Submit(func() {
+		defer func() {
+			if r := recover(); r != nil {
+				workerPool.tasksWg.Done()
+
+				fmt.Println("recovered from panic in WorkerPool:", r)
+			}
+		}()
+
+		f()
+
+		workerPool.tasksWg.Done()
+	}); antsErr != nil {
+		panic(antsErr)
+	}
+}
+
+// Returns the capacity (number of workers) of this pool.
+func (workerPool *WorkerPool) Capacity() int {
+	if workerPool.stopped.IsSet() {
+		return 0
+	}
+
+	return workerPool.get().Cap()
+}
+
+// Changes the capacity of this pool.
+func (workerPool *WorkerPool) Tune(capacity int) {
+	if workerPool.stopped.IsSet() {
+		return
+	}
+
+	workerPool.get().Tune(capacity)
+}
+
+// Returns the available (idle) workers.
+func (workerPool *WorkerPool) IdleWorkers() int {
+	if workerPool.stopped.IsSet() {
+		return 0
+	}
+
+	return workerPool.get().Free()
+}
+
+// Returns the number of the currently running workers.
+func (workerPool *WorkerPool) RunningWorkers() int {
+	if workerPool.stopped.IsSet() {
+		return 0
+	}
+
+	return workerPool.get().Running()
+}
+
+// Immediately closes this pool (see ShutdownGracefully for a method that waits for the scheduled workers to finish).
+func (workerPool *WorkerPool) Shutdown() {
+	workerPool.shutdownOnce.Do(func() {
+		workerPool.stopped.Set()
+
+		if workerPool.pool != nil {
+			workerPool.pool.Release()
+		}
+	})
+}
+
+// Closes this pool and waits for the currently running goroutines to finish.
+func (workerPool *WorkerPool) ShutdownGracefully() {
+	workerPool.shutdownOnce.Do(func() {
+		workerPool.stopped.Set()
+		workerPool.tasksWg.Wait()
+
+		if workerPool.pool != nil {
+			workerPool.pool.Release()
+		}
+	})
+}
+
+// Internal utility function that initiates and returns the pool.
+func (workerPool *WorkerPool) get() *ants.Pool {
+	workerPool.initOnce.Do(func() {
+		if newPool, err := ants.NewPool(runtime.NumCPU()*2, ants.WithNonblocking(false)); err != nil {
+			panic(err)
+		} else {
+			workerPool.pool = newPool
+		}
+	})
+
+	return workerPool.pool
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/dgraph-io/badger v1.6.0
 	github.com/google/open-location-code/go v0.0.0-20190903173953-119bc96a3a51
 	github.com/iotaledger/iota.go v1.0.0-beta.9
+	github.com/panjf2000/ants/v2 v2.2.2 // indirect
 	github.com/pkg/errors v0.8.1
 	github.com/spf13/afero v1.1.2
 	github.com/spf13/pflag v1.0.5
