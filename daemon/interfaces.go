b8de5e2117724d760b16481bc76103c914951bf1
muXxer <mux3r@web.de>
2020-01-02 22:58:42 +0100

    Add daemon interface

---
+++ daemon/interfaces.go
@@ -0,0 +1,35 @@
+package daemon
+
+// A function accepting its shutdown signal handler channel.
+type WorkerFunc = func(shutdownSignal <-chan struct{})
+
+type Daemon interface {
+
+	// GetRunningBackgroundWorkers gets the running background workers.
+	GetRunningBackgroundWorkers() []string
+
+	// BackgroundWorker adds a new background worker to the daemon.
+	// Use order to define in which shutdown order this particular
+	// background worker is shut down (higher = earlier).
+	BackgroundWorker(name string, handler WorkerFunc, order ...int) error
+
+	// Start starts the daemon.
+	Start()
+
+	// Run runs the daemon and then waits for the daemon to shutdown.
+	Run()
+
+	// Shutdown signals all background worker of the daemon shut down.
+	// This call doesn't await termination of the background workers.
+	Shutdown()
+
+	// Shutdown signals all background worker of the daemon to shut down and
+	// then waits for their termination.
+	ShutdownAndWait()
+
+	// IsRunning checks whether the daemon is running.
+	IsRunning() bool
+
+	// IsStopped checks whether the daemon was stopped.
+	IsStopped() bool
+}
