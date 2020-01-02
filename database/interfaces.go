369ffd75d62a47e6e776f8ab38c57a83944af37a
muXxer <mux3r@web.de>
2020-01-02 23:12:40 +0100

    Add new functions to database interface

--- database/interfaces.go
+++ database/interfaces.go
@@ -24,11 +24,16 @@ type Database interface {
 	// Write
 	Set(entry Entry) error
 	Delete(key Key) error
+	DeletePrefix(keyPrefix KeyPrefix) error
 
 	// Iteration
 	ForEach(func(entry Entry) (stop bool)) error
 	ForEachPrefix(keyPrefix KeyPrefix, do func(entry Entry) (stop bool)) error
 	ForEachPrefixKeyOnly(keyPrefix KeyPrefix, do func(entry KeyOnlyEntry) (stop bool)) error
+	StreamForEach(func(entry Entry) error) error
+	StreamForEachKeyOnly(func(entry KeyOnlyEntry) error) error
+	StreamForEachPrefix(keyPrefix KeyPrefix, do func(entry Entry) error) error
+	StreamForEachPrefixKeyOnly(keyPrefix KeyPrefix, do func(entry KeyOnlyEntry) error) error
 
 	// Transactions
 	Apply(set []Entry, delete []Key) error
