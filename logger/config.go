e0fc71bcf72652894a31aac5ac03bb4561c6287b
Wolfgang Welz <welzwo@gmail.com>
2020-01-06 21:05:34 +0100

    feat: switch logger to zap

---
+++ logger/config.go
@@ -0,0 +1,44 @@
+package logger
+
+import "go.uber.org/zap/zapcore"
+
+// Config holds the settings to configure a root logger instance.
+type Config struct {
+	// Level is the minimum enabled logging level.
+	// The default is "info".
+	Level string `mapstructure:"level"`
+	// DisableCaller stops annotating logs with the calling function's file name and line number.
+	// By default, all logs are annotated.
+	DisableCaller bool `mapstructure:"disableCaller"`
+	// DisableStacktrace disables automatic stacktrace capturing.
+	// By default, stacktraces are captured for LevelError and above in production.
+	DisableStacktrace bool `mapstructure:"disableStacktrace"`
+	// Encoding sets the logger's encoding. Valid values are "json" and "console".
+	// The default is "console".
+	Encoding string `mapstructure:"encoding"`
+	// OutputPaths is a list of URLs, file paths or stdout/stderr to write logging output to.
+	// The default is ["stdout"].
+	OutputPaths []string `mapstructure:"outputPaths"`
+	// DisableEvents prevents log messages from being raced as events.
+	// By default, the corresponding events are triggered.
+	DisableEvents bool `mapstructure:"disableEvents"`
+}
+
+var defaultCfg = Config{
+	Level:       "info",
+	Encoding:    "console",
+	OutputPaths: []string{"stdout"},
+}
+
+var defaultEncoderConfig = zapcore.EncoderConfig{
+	TimeKey:        "ts",
+	LevelKey:       "level",
+	NameKey:        "logger",
+	CallerKey:      "caller",
+	MessageKey:     "msg",
+	StacktraceKey:  "stacktrace",
+	EncodeLevel:    zapcore.CapitalLevelEncoder,    // level in upper case
+	EncodeTime:     zapcore.RFC3339TimeEncoder,     // timestamp according to RFC3339
+	EncodeDuration: zapcore.SecondsDurationEncoder, // duration in seconds
+	EncodeCaller:   zapcore.ShortCallerEncoder,     // caller according to package/file:line
+}
---
+++ logger/events_test.go
@@ -0,0 +1,94 @@
+package logger
+
+import (
+	"testing"
+
+	"github.com/iotaledger/hive.go/events"
+	"github.com/magiconair/properties/assert"
+	"github.com/stretchr/testify/mock"
+	"go.uber.org/zap"
+)
+
+const (
+	testName      = "test"
+	testMsg       = "123"
+	testLoggedMsg = testMsg
+)
+
+func TestNewEventCore(t *testing.T) {
+	// initialize the mock
+	m, teardown := newEventMock(t)
+
+	t.Run("debugLevel", func(t *testing.T) {
+		logger := zap.New(NewEventCore(LevelDebug))
+
+		// there should not be any events for debug-level logs.
+		logger.Debug(testMsg)
+	})
+
+	t.Run("levelDisabled", func(t *testing.T) {
+		logger := zap.New(NewEventCore(LevelWarn))
+
+		// there should not be any events, as info is below warning.
+		logger.Info(testMsg)
+	})
+
+	t.Run("eventsTriggered", func(t *testing.T) {
+		logger := zap.New(NewEventCore(LevelInfo)).Named(testName)
+
+		m.On("info", LevelInfo, testName, testLoggedMsg).Once()
+		m.On("any", LevelInfo, testName, testLoggedMsg).Once()
+		logger.Info(testMsg)
+
+		m.On("warn", LevelWarn, testName, testLoggedMsg).Once()
+		m.On("any", LevelWarn, testName, testLoggedMsg).Once()
+		logger.Warn(testMsg)
+
+		m.On("error", LevelError, testName, testLoggedMsg).Once()
+		m.On("any", LevelError, testName, testLoggedMsg).Once()
+		logger.Error(testMsg)
+
+		m.On("panic", LevelPanic, testName, testLoggedMsg).Once()
+		m.On("any", LevelPanic, testName, testLoggedMsg).Once()
+		assert.Panic(t, func() { logger.Panic(testMsg) }, testMsg)
+
+		m.AssertExpectations(t)
+	})
+
+	// remove the mock
+	teardown()
+}
+
+type eventMock struct{ mock.Mock }
+
+func (e *eventMock) info(lvl Level, name string, msg string)  { e.Called(lvl, name, msg) }
+func (e *eventMock) warn(lvl Level, name string, msg string)  { e.Called(lvl, name, msg) }
+func (e *eventMock) error(lvl Level, name string, msg string) { e.Called(lvl, name, msg) }
+func (e *eventMock) panic(lvl Level, name string, msg string) { e.Called(lvl, name, msg) }
+func (e *eventMock) any(lvl Level, name string, msg string)   { e.Called(lvl, name, msg) }
+
+func newEventMock(t *testing.T) (*eventMock, func()) {
+	m := &eventMock{}
+	m.Test(t)
+
+	infoC := events.NewClosure(m.info)
+	warnC := events.NewClosure(m.warn)
+	errorC := events.NewClosure(m.error)
+	panicC := events.NewClosure(m.panic)
+	anyC := events.NewClosure(m.any)
+
+	Events.InfoMsg.Attach(infoC)
+	Events.WarningMsg.Attach(warnC)
+	Events.ErrorMsg.Attach(errorC)
+	Events.PanicMsg.Attach(panicC)
+	Events.AnyMsg.Attach(anyC)
+
+	teardown := func() {
+		Events.InfoMsg.Detach(infoC)
+		Events.WarningMsg.Detach(warnC)
+		Events.ErrorMsg.Detach(errorC)
+		Events.PanicMsg.Detach(panicC)
+		Events.AnyMsg.Detach(anyC)
+	}
+	return m, teardown
+}
