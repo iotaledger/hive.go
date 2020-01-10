09629975f1f635e144189f7a325b344ba4ae67b2
Wolfgang Welz <welzwo@gmail.com>
2020-01-10 12:26:39 +0100

    fix: correct flags

--- node/parameters.go
+++ node/parameters.go
@@ -1,18 +1,15 @@
 package node
 
 import (
-	"github.com/iotaledger/hive.go/logger"
 	flag "github.com/spf13/pflag"
 )
 
 const (
-	CFG_LOG_LEVEL       = "node.LogLevel"
 	CFG_DISABLE_PLUGINS = "node.disablePlugins"
 	CFG_ENABLE_PLUGINS  = "node.enablePlugins"
 )
 
 func init() {
-	flag.Int(CFG_LOG_LEVEL, int(logger.LevelInfo), "controls the log types that are shown")
-	flag.String(CFG_DISABLE_PLUGINS, "", "a list of plugins that shall be disabled")
-	flag.String(CFG_ENABLE_PLUGINS, "", "a list of plugins that shall be enabled")
+	flag.StringSlice(CFG_DISABLE_PLUGINS, nil, "a list of plugins that shall be disabled")
+	flag.StringSlice(CFG_ENABLE_PLUGINS, nil, "a list of plugins that shall be enabled")
 }
