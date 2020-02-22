f4d56f6e4a770656d4a0684870070c90bbf47e22
Hans Moog <hm@mkjc.net>
2020-02-22 02:26:46 +0100

    Feat: made run optional when creating a Plugin

--- parameter/parameter.go
+++ parameter/parameter.go
@@ -5,15 +5,9 @@ import (
 	"fmt"
 	"log"
 	"strings"
-	"sync"
 
 	flag "github.com/spf13/pflag"
 	"github.com/spf13/viper"
-)
-
-var (
-	defaultConfig     *viper.Viper
-	defaultConfigInit sync.Once
 )
 
 // PrintConfig prints the actual configuration, ignoreSettingsAtPrint are not shown
@@ -36,21 +30,6 @@ func PrintConfig(config *viper.Viper, ignoreSettingsAtPrint ...[]string) {
 	if cfg, err := json.MarshalIndent(settings, "", "  "); err == nil {
 		fmt.Printf("Parameters loaded: \n %+v\n", string(cfg))
 	}
-}
-
-func DefaultConfig() *viper.Viper {
-	defaultConfigInit.Do(func() {
-		configName := *flag.StringP("config", "c", "config", "Filename of the config file without the file extension")
-		configDirPath := *flag.StringP("config-dir", "d", ".", "Path to the directory containing the config file")
-
-		defaultConfig = viper.New()
-		err := LoadConfigFile(defaultConfig, configDirPath, configName, true, true)
-		if err != nil {
-			log.Panicf("Error loading config: %s", err)
-		}
-	})
-
-	return defaultConfig
 }
 
 // LoadConfigFile fetches config values from a dir defined in "configDir" (or the current working dir if not set)
