f906f3bec8bcf13c2a76cb9d2daceae6573e0564
Hans Moog <hm@mkjc.net>
2020-02-27 16:57:01 +0100

    Feat: added unit test to retainedpartitions

--- objectstorage/test/retainedpartition_test.go
+++ objectstorage/test/retainedpartition_test.go
@@ -1,8 +1,9 @@
 package test
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 
 	"github.com/iotaledger/hive.go/objectstorage"
 )
@@ -10,8 +11,17 @@ import (
 func TestRetainedPartition(t *testing.T) {
 	retainedPartition := objectstorage.NewRetainedPartition()
 
+	assert.Equal(t, false, retainedPartition.IsRetained([]string{"A"}))
+	assert.Equal(t, false, retainedPartition.IsRetained([]string{"A", "B"}))
+
 	retainedPartition.Retain([]string{"A", "B"})
+
+	assert.Equal(t, true, retainedPartition.IsRetained([]string{"A"}))
+	assert.Equal(t, true, retainedPartition.IsRetained([]string{"A", "B"}))
+	assert.Equal(t, false, retainedPartition.IsRetained([]string{"A", "B", "C"}))
+
 	retainedPartition.Release([]string{"A", "B"})
 
-	fmt.Println(retainedPartition.IsRetained([]string{"A", "B"}))
+	assert.Equal(t, false, retainedPartition.IsRetained([]string{"A"}))
+	assert.Equal(t, false, retainedPartition.IsRetained([]string{"A", "B"}))
 }
