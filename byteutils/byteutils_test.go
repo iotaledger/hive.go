f7b4dfa0aa09fc04a7bd2372371fc1938dc9a1fb
Hans Moog <hm@mkjc.net>
2020-08-17 19:59:50 +0200

    Feat: renamed test

--- byteutils/byteutils_test.go
+++ byteutils/byteutils_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestConcat(t *testing.T) {
+func TestConcatBytes(t *testing.T) {
 	assert.Equal(t, []byte{1, 2, 3, 4, 5, 7, 8}, ConcatBytes([]byte{1, 2, 3}, []byte{4, 5}, []byte{7, 8}))
 	assert.Equal(t, []byte{1, 2, 3, 4, 5, 7, 8}, ConcatBytes([]byte{1, 2, 3, 4, 5, 7, 8}))
 }
