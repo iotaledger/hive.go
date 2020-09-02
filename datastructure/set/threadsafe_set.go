23338747dff6e8234f7ab71c2c1bdd4c66f8cf93
Hans Moog <hm@mkjc.net>
2020-09-02 21:10:35 +0200

    Feat: implemented foreach

--- datastructure/set/threadsafe_set.go
+++ datastructure/set/threadsafe_set.go
@@ -1,33 +1,97 @@
 package set
 
-type threadSafeSet struct{}
+import (
+	"sync"
+
+	"github.com/iotaledger/hive.go/types"
+)
+
+type threadSafeSet struct {
+	elements      map[interface{}]types.Empty
+	elementsMutex sync.RWMutex
+}
 
 func newThreadSafeSet() *threadSafeSet {
-	return &threadSafeSet{}
+	return &threadSafeSet{
+		elements: make(map[interface{}]types.Empty),
+	}
 }
 
 func (set *threadSafeSet) Add(element interface{}) bool {
-	panic("implement me")
+	set.elementsMutex.RLock()
+	if _, elementExists := set.elements[element]; elementExists {
+		set.elementsMutex.RUnlock()
+
+		return false
+	}
+
+	set.elementsMutex.RUnlock()
+	set.elementsMutex.Lock()
+	defer set.elementsMutex.Unlock()
+
+	if _, elementExists := set.elements[element]; elementExists {
+		return false
+	}
+
+	set.elements[element] = types.Void
+
+	return true
 }
 
 func (set *threadSafeSet) Delete(element interface{}) bool {
-	panic("implement me")
+	set.elementsMutex.RLock()
+	if _, exists := set.elements[element]; !exists {
+		set.elementsMutex.RUnlock()
+
+		return false
+	}
+	set.elementsMutex.RUnlock()
+
+	set.elementsMutex.Lock()
+	defer set.elementsMutex.Unlock()
+	if _, exists := set.elements[element]; !exists {
+		return false
+	}
+
+	delete(set.elements, element)
+
+	return true
 }
 
 func (set *threadSafeSet) Has(element interface{}) bool {
-	panic("implement me")
+	set.elementsMutex.RLock()
+	defer set.elementsMutex.RUnlock()
+
+	_, exists := set.elements[element]
+
+	return exists
 }
 
 func (set *threadSafeSet) ForEach(callback func(element interface{})) {
-	panic("implement me")
+	set.elementsMutex.RLock()
+	copiedElements := make(map[interface{}]types.Empty)
+	for element := range set.elements {
+		copiedElements[element] = types.Void
+	}
+	set.elementsMutex.RUnlock()
+
+	for element := range copiedElements {
+		callback(element)
+	}
 }
 
 func (set *threadSafeSet) Clear() {
-	panic("implement me")
+	set.elementsMutex.Lock()
+	defer set.elementsMutex.Unlock()
+
+	set.elements = make(map[interface{}]types.Empty)
 }
 
 func (set *threadSafeSet) Size() int {
-	panic("implement me")
+	set.elementsMutex.RLock()
+	defer set.elementsMutex.RUnlock()
+
+	return len(set.elements)
 }
 
 // code contract - make sure the type implements the interface
