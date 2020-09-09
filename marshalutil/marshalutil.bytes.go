739158b5dd3d226a2acb69cddc3d8e017d22f432
Hans Moog <hm@mkjc.net>
2020-09-10 01:19:08 +0200

    Feat: added the ability to read a byte range

--- marshalutil/marshalutil.bytes.go
+++ marshalutil/marshalutil.bytes.go
@@ -1,5 +1,9 @@
 package marshalutil
 
+import (
+	"github.com/iotaledger/hive.go/byteutils"
+)
+
 // WriteBytes appends the given bytes to the internal buffer.
 // It returns the same MarshalUtil so calls can be chained.
 func (util *MarshalUtil) WriteBytes(bytes []byte) *MarshalUtil {
@@ -16,20 +20,33 @@ func (util *MarshalUtil) WriteBytes(bytes []byte) *MarshalUtil {
 	return util
 }
 
-// ReadBytes unmarshals the given amount of bytes from the internal read buffer.
-func (util *MarshalUtil) ReadBytes(length int) ([]byte, error) {
-	if length < 0 {
-		length = len(util.bytes) - util.readOffset + length
+// ReadBytes unmarshals the given amount of bytes from the internal read buffer and advances the read offset.
+func (util *MarshalUtil) ReadBytes(length int, optionalReadOffset ...int) ([]byte, error) {
+	// determine the read offset
+	readOffset := util.readOffset
+	if len(optionalReadOffset) >= 1 {
+		readOffset = optionalReadOffset[0]
 	}
 
+	// determine the length
+	if length < 0 {
+		length = len(util.bytes) - readOffset + length
+	}
+
+	// calculate the end offset
 	readEndOffset, err := util.checkReadCapacity(length)
 	if err != nil {
 		return nil, err
 	}
 
-	defer util.ReadSeek(readEndOffset)
+	// return a copy of the byte range if a manual offset was provided
+	if len(optionalReadOffset) != 0 {
+		return byteutils.ConcatBytes(util.bytes[readOffset:readEndOffset]), nil
+	}
 
-	return util.bytes[util.readOffset:readEndOffset], nil
+	// advance read offset and return read bytes
+	util.ReadSeek(readEndOffset)
+	return util.bytes[readOffset:readEndOffset], nil
 }
 
 func (util *MarshalUtil) ReadRemainingBytes() []byte {
