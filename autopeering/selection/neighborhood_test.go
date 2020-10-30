2776388234c07076144b448dd24e0d6c4520fd54
Hans Moog <hm@mkjc.net>
2020-10-30 14:53:13 +0100

    Feat: reverted erroneous changes

--- autopeering/selection/neighborhood_test.go
+++ autopeering/selection/neighborhood_test.go
@@ -139,7 +139,7 @@ func TestRemove(t *testing.T) {
 
 	for _, test := range tests {
 		test.input.RemovePeer(test.toRemove.ID())
-		assert.Equal(t, test.expected, test.input.neighbors, "Delete")
+		assert.Equal(t, test.expected, test.input.neighbors, "Remove")
 	}
 }
 
