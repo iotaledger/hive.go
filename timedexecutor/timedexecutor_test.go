cb935d64415d8d3e6bc3cd724bc11215279455d8
Hans Moog <3293976+hmoog@users.noreply.github.com>
2021-02-25 00:37:15 +0100

    Fix: Memory leak in timedqueue (#224)
    * Feat: started cleaning up the objectstorage a bit
    
    * Feat: further refactor
    
    * Refactor: reverted changes due to scheduledTask reset
    
    * Fix: fixed a memleak in the timedQueue
    
    * Refactor: cleaned up test
    
    Co-authored-by: Hans Moog <hm@mkjc.net>

---
+++ timedexecutor/timedexecutor_test.go
@@ -0,0 +1,42 @@
+package timedexecutor
+
+import (
+	"runtime"
+	"runtime/debug"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTimedExecutor_MemLeak(t *testing.T) {
+	const testCount = 100000
+
+	timedExecutor := New(1)
+	memStatsStart := memStats()
+
+	var executionCounter uint64
+	for i := 0; i < testCount; i++ {
+		timedExecutor.ExecuteAfter(func() {
+			atomic.AddUint64(&executionCounter, 1)
+		}, 500*time.Millisecond)
+	}
+
+	assert.Eventually(t, func() bool {
+		return atomic.LoadUint64(&executionCounter) == uint64(testCount)
+	}, 10*time.Second, 100*time.Millisecond)
+
+	memStatsEnd := memStats()
+	assert.Less(t, float64(memStatsEnd.HeapObjects), 1.1*float64(memStatsStart.HeapObjects), "the objects in the heap should not grow by more than 10%")
+}
+
+func memStats() *runtime.MemStats {
+	runtime.GC()
+	debug.FreeOSMemory()
+
+	var memStats runtime.MemStats
+	runtime.ReadMemStats(&memStats)
+
+	return &memStats
+}
---
+++ timedqueue/timedqueue_test.go
@@ -0,0 +1,48 @@
+package timedqueue
+
+import (
+	"runtime"
+	"runtime/debug"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestQueueElement_MemLeak(t *testing.T) {
+	const testCount = 100000
+
+	timedQueue := New()
+	memStatsStart := memStats()
+
+	go func() {
+		for currentElement := timedQueue.Poll(true); currentElement != nil; currentElement = timedQueue.Poll(true) {
+			currentElement.(func())()
+		}
+	}()
+
+	var executionCounter uint64
+	for i := 0; i < testCount; i++ {
+		timedQueue.Add(func() {
+			atomic.AddUint64(&executionCounter, 1)
+		}, time.Now().Add(500*time.Millisecond))
+	}
+
+	assert.Eventually(t, func() bool {
+		return atomic.LoadUint64(&executionCounter) == uint64(testCount)
+	}, 10*time.Second, 100*time.Millisecond)
+
+	memStatsEnd := memStats()
+	assert.Less(t, float64(memStatsEnd.HeapObjects), 1.1*float64(memStatsStart.HeapObjects), "the objects in the heap should not grow by more than 10%")
+}
+
+func memStats() *runtime.MemStats {
+	runtime.GC()
+	debug.FreeOSMemory()
+
+	var memStats runtime.MemStats
+	runtime.ReadMemStats(&memStats)
+
+	return &memStats
+}
