1ac328c4054fafbf37d14dd3a94bb387e745535d
Hans Moog <3293976+hmoog@users.noreply.github.com>
2021-03-17 19:51:22 +0100

    Feat: Feat: Add UpdateBoundParameters (#240)
    * Feat: Feat: Add UpdateBoundParameters
    
    * Refactor: changed shorthand to avoid collision with other tests
    
    Co-authored-by: Hans Moog <hm@mkjc.net>

--- configuration/utils_test.go
+++ configuration/utils_test.go
@@ -1,11 +1,44 @@
 package configuration
 
 import (
+	"os"
 	"testing"
 
 	flag "github.com/spf13/pflag"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestUpdateBoundParameters(t *testing.T) {
+	var parameters = struct {
+		TestField int64 `shorthand:"q" default:"13" usage:"you can do stuff with this parameter"`
+	}{}
+
+	BindParameters(&parameters, "test")
+
+	os.Setenv("test_test.testField", "321")
+
+	testConfig := New()
+	if err := testConfig.LoadFlagSet(flag.CommandLine); err != nil {
+		panic(err)
+	}
+
+	// read in ENV variables
+	// load the env vars after default values from flags were set (otherwise the env vars are not added because the keys don't exist)
+	if err := testConfig.LoadEnvironmentVars("test"); err != nil {
+		panic(err)
+	}
+
+	// load the flags again to overwrite env vars that were also set via command line
+	if err := testConfig.LoadFlagSet(flag.CommandLine); err != nil {
+		panic(err)
+	}
+
+	assert.False(t, parameters.TestField == testConfig.Int64("test.testField"))
+
+	UpdateBoundParameters(testConfig)
+
+	assert.True(t, parameters.TestField == testConfig.Int64("test.testField"))
+}
 
 func TestBindParameters(t *testing.T) {
 	var parameters = struct {
