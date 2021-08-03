23a9f7c3b9b6ffb8fbe9a1ad6dc8f30c7c1983b9
lunfardo314 <evaldas.drasutis@iota.org>
2021-08-03 12:07:11 +0300

    readme

--- crypto/kzg/doc.go
+++ crypto/kzg/doc.go
@@ -2,19 +2,19 @@
 // SPDX-License-Identifier: Apache-2.0
 
 // Package kzg implements functions needed for Kate-Zaverucha-Goldberg polynomial commitments,
-// also know as KZG, KZG10 and Kate commitments.
+// also known as KZG, KZG10 and Kate commitments.
 // The KZG commitments are needed for vector commitments and the implementation
 // of 'verkle' trees, a more efficient variation of a classic Merkle trees.
 // See:
 // - https://www.iacr.org/archive/asiacrypt2010/6477178/6477178.pdf
 // - https://hackmd.io/@tompocock/Hk2A7BD6U
-// The implementation uses DEDIS Advanced Crypto Library for Go Kyber v.3 and its
+// The implementation uses DEDIS Advanced Crypto Library for Go Kyber v3 and its
 // BN256 bilinear pairing suite as cryptographic primitives.
 // The implementation assumes fixed degree of polynomials D = 16.
 // It follows guidelines:
 // - https://dankradfeist.de/ethereum/2020/06/16/kate-polynomial-commitments.html
 // - https://dankradfeist.de/ethereum/2021/06/18/pcs-multiproofs.html
-// However, the KZG package uses different structure of trusted setup than proposed.
+// This KZG package uses proprietary structure of trusted setup.
 // The trusted setup contains different values on G1 curve precomputed from the secret scalar and
-// geneated (net secret) primitive root of unity for the field.
+// generated (net secret) primitive root of unity for the field.
 package kzg
---
+++ crypto/kzg/readme.md
@@ -0,0 +1,27 @@
+# Package KZG
+Package KZG implements functions needed for _Kate-Zaverucha-Goldberg_ polynomial commitments,
+also known as KZG, KZG10 and **Kate commitments**.
+
+The KZG commitments are needed to calculate vector committments for the implementation
+of `verkle trees`, a more efficient variation of a classic Merkle trees.
+
+See:
+* [Constant-Size Commitments to Polynomials and Their Applications](https://www.iacr.org/archive/asiacrypt2010/6477178/6477178.pdf),
+  the original paper
+
+The implementation uses [DEDIS Advanced Crypto Library for Go Kyber v3](https://github.com/dedis/kyber)
+and its `BN256` bilinear pairing suite as cryptographic primitives.
+
+The implementation assumes fixed degree of polynomials D = 16. It follows guidelines:
+* [KZG polynomial commitments](https://dankradfeist.de/ethereum/2020/06/16/kate-polynomial-commitments.html) by Dankrad Feist
+* [PCS multiproofs using random evaluation](https://dankradfeist.de/ethereum/2021/06/18/pcs-multiproofs.html) Dankrad Feist
+
+However, this implementation uses proprietary structure of the trusted setup.
+The implemented trusted setup contains different values on G1 curve which are precomputed from the secret scalar and
+generated (not secret) primitive root of unity for the field.
+
+Some more readings:
+
+* [Kate Commitments: A Primer](https://hackmd.io/@tompocock/Hk2A7BD6U)
+
+
