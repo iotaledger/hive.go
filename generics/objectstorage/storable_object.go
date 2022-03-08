069d85ce0194c15d1629bfd8fd6daf46abb8b87a
Piotr Macek <piotr.macek@iota.org>
2022-03-08 14:40:16 +0100

    Update generic storable object interface.

--- generics/objectstorage/storable_object.go
+++ generics/objectstorage/storable_object.go
@@ -3,7 +3,6 @@ package objectstorage
 import "github.com/iotaledger/hive.go/objectstorage"
 
 type StorableObject interface {
-	FromBytes(bytes []byte) (StorableObject, error)
 	FromObjectStorage(key, data []byte) (StorableObject, error)
 
 	objectstorage.StorableObject
