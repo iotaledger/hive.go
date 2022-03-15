7c704861bee31f1a4f9f058290f595bae5062b1d
Hans Moog <3293976+hmoog@users.noreply.github.com>
2022-03-15 15:09:44 +0100

    Feat: refactored code

--- generics/dataflow/dataflow.go
+++ generics/dataflow/dataflow.go
@@ -4,7 +4,7 @@ import (
 	"github.com/iotaledger/hive.go/generics/stack"
 )
 
-type ChainedCommand[T any] func(param T, next func(param T) error) error
+// region DataFlow /////////////////////////////////////////////////////////////////////////////////////////////////////
 
 type Dataflow[T any] struct {
 	steps []ChainedCommand[T]
@@ -20,32 +20,44 @@ func (d *Dataflow[T]) Run(param T) error {
 	return newCallstack[T](d.steps...).call(param)
 }
 
+// endregion ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// region ChainedCommand ///////////////////////////////////////////////////////////////////////////////////////////////
+
+type ChainedCommand[T any] func(param T, next func(param T) error) error
+
+// endregion ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// region callStack ////////////////////////////////////////////////////////////////////////////////////////////////////
+
 type callstack[T any] struct {
-	chainedCommands stack.Stack[ChainedCommand[T]]
+	stack.Stack[ChainedCommand[T]]
 }
 
-func newCallstack[T any](steps ...ChainedCommand[T]) *callstack[T] {
-	chainedCommands := stack.New[ChainedCommand[T]](false)
+func newCallstack[T any](steps ...ChainedCommand[T]) (newCallStack *callstack[T]) {
+	return (&callstack[T]{stack.New[ChainedCommand[T]](false)}).addSteps(steps...)
+}
 
+func (n *callstack[T]) addSteps(steps ...ChainedCommand[T]) (self *callstack[T]) {
 	stepCount := len(steps)
 	for i := 0; i < stepCount; i++ {
-		chainedCommands.Push(steps[stepCount-i-1])
+		n.Push(steps[stepCount-i-1])
 	}
 
-	return &callstack[T]{
-		chainedCommands: chainedCommands,
-	}
+	return n
 }
 
 func (n *callstack[T]) call(param T) (err error) {
-	if n.chainedCommands.IsEmpty() {
+	if n.IsEmpty() {
 		return nil
 	}
 
-	step, exists := n.chainedCommands.Pop()
+	step, exists := n.Pop()
 	if !exists {
 		return nil
 	}
 
 	return step(param, n.call)
 }
+
+// endregion ///////////////////////////////////////////////////////////////////////////////////////////////////////////
