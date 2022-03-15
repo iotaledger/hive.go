cecf57d2538e7c64fbef99f251eb511ecf107aaa
Hans Moog <3293976+hmoog@users.noreply.github.com>
2022-03-15 21:44:13 +0100

    Feat: added comment for DataFlow

--- generics/dataflow/dataflow.go
+++ generics/dataflow/dataflow.go
@@ -2,43 +2,64 @@ package dataflow
 
 // region DataFlow /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-type Dataflow[T any] []ChainedCommand[T]
-
-func New[T any](steps ...ChainedCommand[T]) (dataFlow Dataflow[T]) {
-	return steps
+// DataFlow represents a chain of commands where the next command gets executed by the previous one passing on a common
+// object holding the shared state. The recursive nature of the calls causes acquired resources (e.g. CachedObjects) to
+// be held until the full data flow terminates.
+//
+// This allows us to pass through unwrapped objects in most of the business logic which relaxes the stress on the
+// caching layer and makes the code less verbose.
+type DataFlow[T any] struct {
+	step     int
+	max      int
+	commands []ChainedCommand[T]
 }
 
-func (d Dataflow[T]) Run(param T) error {
-	return (&d).call(param)
-}
-
-func (d Dataflow[T]) ChainedCommand() ChainedCommand[T] {
-	return func(param T, next func(param T) error) error {
-		return (append(d, func(param T, _ func(param T) error) error {
-			return next(param)
-		})).Run(param)
+// New creates a new DataFlow from the given ChainedCommands.
+func New[T any](steps ...ChainedCommand[T]) (dataFlow *DataFlow[T]) {
+	return &DataFlow[T]{
+		commands: steps,
+		max:      len(steps),
+		step:     -1,
 	}
 }
 
-func (d *Dataflow[T]) call(param T) (err error) {
-	if len(*d) == 0 {
+// Run executes the DataFlow with the given parameter. It aborts execution and returns an error if any of the chained
+// commands returns an error.
+//
+// Note: A DataFlow can only be run a single.
+func (d *DataFlow[T]) Run(param T) error {
+	d.step++
+	if d.step >= d.max {
 		return nil
 	}
 
-	return d.nextCommand()(param, d.call)
+	return d.commands[d.step](param, d.Run)
 }
 
-func (d *Dataflow[T]) nextCommand() (nextCommand ChainedCommand[T]) {
-	nextCommand = (*d)[0]
-	*d = (*d)[1:]
+// RunWithCallback executes the DataFlow with the given parameter and executes a callback if the execution succeeds.
+// It aborts execution and returns an error if any of the chained commands returns an error.
+//
+// RunWithCallback is a ChainedCommand itself and the method (without calling it) can be passed into other DataFlows to
+// enable composition.
+//
+// Note: A DataFlow can only be run a single.
+func (d *DataFlow[T]) RunWithCallback(param T, callback func(param T) error) error {
+	d.commands = append(d.commands, func(param T, _ func(param T) error) error {
+		return callback(param)
+	})
+	d.max++
 
-	return nextCommand
+	return d.Run(param)
 }
+
+// code contract (ensure that the RunWithCallback function is a ChainedCommand which enables composition of DataFlows).
+var _ ChainedCommand[int] = new(DataFlow[int]).RunWithCallback
 
 // endregion ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // region ChainedCommand ///////////////////////////////////////////////////////////////////////////////////////////////
 
+// ChainedCommand represents the interface for the callbacks used in a DataFlow.
 type ChainedCommand[T any] func(param T, next func(param T) error) error
 
 // endregion ///////////////////////////////////////////////////////////////////////////////////////////////////////////
--- generics/dataflow/dataflow_test.go
+++ generics/dataflow/dataflow_test.go
@@ -1,38 +1,56 @@
 package dataflow
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/cockroachdb/errors"
+	"github.com/stretchr/testify/assert"
 )
 
-func Test(t *testing.T) {
+func Benchmark(b *testing.B) {
 	x := func(param int, next func(int) error) error {
-		fmt.Println("x START", param)
-		defer fmt.Println("x END")
-
 		return next(param + 1)
 	}
 
 	y := func(param int, next func(int) error) error {
-		fmt.Println("y START", param)
-		defer fmt.Println("y END")
-
 		return next(param + 2)
 	}
 
 	z := func(param int, next func(int) error) error {
-		fmt.Println("z START", param)
-		defer fmt.Println("z END")
-
 		return errors.Errorf("FAILED")
 	}
 
-	dataFlow1 := New(x, y)
-	fmt.Println(dataFlow1.Run(1))
-	fmt.Println(dataFlow1.Run(7))
+	dataFlow1 := New(x, y, z)
 
-	dataFlow2 := New(dataFlow1.ChainedCommand(), z)
-	fmt.Println(dataFlow2.Run(1))
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		dataFlow1.Run(i)
+	}
+}
+
+func Test(t *testing.T) {
+	x := func(param int, next func(int) error) error {
+		return next(param + 1)
+	}
+
+	y := func(param int, next func(int) error) error {
+		return next(param + 2)
+	}
+
+	z := func(param int, next func(int) error) error {
+		return errors.Errorf("FAILED")
+	}
+
+	result := 0
+	assert.NoError(t, New(x, y, func(param int, next func(result int) error) error {
+		result = param
+
+		return next(param)
+	}).Run(1))
+	assert.NoError(t, New(x, y).Run(7))
+	assert.Equal(t, 4, result)
+
+	dataFlow2 := New(New(x, y).RunWithCallback, y, z)
+	assert.Error(t, dataFlow2.Run(1))
 }
