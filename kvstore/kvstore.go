ba5059d9f33bb7d6e00673f55700c6341e1f3407
muXxer <git@muxxer.de>
2022-04-04 12:00:21 +0200

    Add comment

--- kvstore/kvstore.go
+++ kvstore/kvstore.go
@@ -8,6 +8,7 @@ import (
 var (
 	// ErrKeyNotFound is returned when an op. doesn't find the given key.
 	ErrKeyNotFound = errors.New("key not found")
+	// ErrStoreClosed is returned when an op accesses the kvstore but it was already closed.
 	ErrStoreClosed = errors.New("trying to access closed kvstore")
 
 	EmptyPrefix = KeyPrefix{}
