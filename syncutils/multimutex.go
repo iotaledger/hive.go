d344e455dd239e17751177eb738d62e17878bf83
Jonas Theis <4181434+jonastheis@users.noreply.github.com>
2022-04-08 14:05:40 +0100

    Improve multimutex (#322)
    * Improve readability and add test and benchmark
    
    * Optimize code and add better benchmark

--- syncutils/multimutex.go
+++ syncutils/multimutex.go
@@ -2,76 +2,71 @@ package syncutils
 
 import (
 	"sync"
+
+	"github.com/iotaledger/hive.go/types"
 )
 
 // region MultiMutex ///////////////////////////////////////////////////////////////////////////////////////////////////
 
+// MultiMutex is a mutex that allows to lock multiple entities exclusively.
+// Entities are represented by interface{} identifiers and can be presented in arbitrary order.
+// Goroutine 1        Goroutine 2        Goroutine 3
+//   Lock(a,c)           		-          Lock(c,b) <- blocking
+//        work            Lock(b)   	        wait
+// Unlock(a,c)               work   	        wait
+//           -          Unlock(b)   	        wait
+//           -                  -   	   Lock(c,b) <- successful
 type MultiMutex struct {
-	locks     map[interface{}]empty
+	locks     map[interface{}]types.Empty
 	locksCond *sync.Cond
-	initOnce  sync.Once
 }
 
+// NewMultiMutex creates a new MultiMutex.
 func NewMultiMutex() *MultiMutex {
 	return &MultiMutex{
-		locks: make(map[interface{}]empty),
-		locksCond: &sync.Cond{
-			L: &sync.Mutex{},
-		},
+		locks:     make(map[interface{}]types.Empty),
+		locksCond: sync.NewCond(&sync.Mutex{}),
 	}
 }
 
-// LockEntity locks all locks that are required for the given LockEntity.
-func (mutex *MultiMutex) LockEntity(entity LockableEntity) {
-	mutex.Lock(entity.Locks()...)
+// LockEntity locks all locks that are required for the given LockableEntity.
+func (m *MultiMutex) LockEntity(entity LockableEntity) {
+	m.Lock(entity.Locks()...)
 }
 
-// UnlockEntity unlocks all locks that are required for the given LockEntity.
-func (mutex *MultiMutex) UnlockEntity(entity LockableEntity) {
-	mutex.Unlock(entity.Locks()...)
+// UnlockEntity unlocks all locks that are required for the given LockableEntity.
+func (m *MultiMutex) UnlockEntity(entity LockableEntity) {
+	m.Unlock(entity.Locks()...)
 }
 
-func (mutex *MultiMutex) Lock(identifiers ...interface{}) {
-	mutex.initOnce.Do(func() {
-		mutex.locks = make(map[interface{}]empty)
-		mutex.locksCond = &sync.Cond{L: &sync.Mutex{}}
-	})
+// Lock blocks until all locks given by ids can be acquired atomically.
+func (m *MultiMutex) Lock(ids ...interface{}) {
+	m.locksCond.L.Lock()
 
-	mutex.locksCond.L.Lock()
-
-AcquireLocks:
-	for i, identifier := range identifiers {
-		if _, isLocked := mutex.locks[identifier]; !isLocked {
-			mutex.locks[identifier] = void
-		} else {
-			for j := 0; j < i; j++ {
-				delete(mutex.locks, identifiers[j])
-			}
-
-			if i > 0 {
-				mutex.locksCond.Broadcast()
-			}
-			mutex.locksCond.Wait()
-
-			goto AcquireLocks
+TryAcquireLocks:
+	for _, id := range ids {
+		if _, exists := m.locks[id]; exists {
+			m.locksCond.Wait()
+			goto TryAcquireLocks
 		}
 	}
 
-	mutex.locksCond.L.Unlock()
+	// Acquire all locks.
+	for _, id := range ids {
+		m.locks[id] = types.Void
+	}
+
+	m.locksCond.L.Unlock()
 }
 
-func (mutex *MultiMutex) Unlock(identifiers ...interface{}) {
-	mutex.initOnce.Do(func() {
-		mutex.locks = make(map[interface{}]empty)
-		mutex.locksCond = &sync.Cond{L: &sync.Mutex{}}
-	})
-
-	mutex.locksCond.L.Lock()
-	for _, identifier := range identifiers {
-		delete(mutex.locks, identifier)
+// Unlock releases the locks of ids.
+func (m *MultiMutex) Unlock(ids ...interface{}) {
+	m.locksCond.L.Lock()
+	for _, id := range ids {
+		delete(m.locks, id)
 	}
-	mutex.locksCond.L.Unlock()
-	mutex.locksCond.Broadcast()
+	m.locksCond.L.Unlock()
+	m.locksCond.Broadcast()
 }
 
 // endregion ///////////////////////////////////////////////////////////////////////////////////////////////////////////
