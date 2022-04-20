23840c0384d4f087b25841f92432bb3d7ae1b585
Georgy Savva <georgysavva@pm.me>
2022-04-20 08:52:17 +0400

    Add support for int* and float* types

--- serix/decode.go
+++ serix/decode.go
@@ -121,7 +121,9 @@ func (api *API) decodeBasedOnType(ctx context.Context, b []byte, value reflect.V
 		})
 		return deseri.Done()
 
-	case reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+	case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+		reflect.Float32, reflect.Float64:
 		deseri := serializer.NewDeserializer(b)
 		addrValue := value.Addr()
 		_, addrTypeToConvert := getNumberTypeToConvert(valueType.Kind())
