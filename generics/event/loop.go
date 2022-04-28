d5f003d53f84f1ad2d04459c792f5190eb23f0fd
Hans Moog <3293976+hmoog@users.noreply.github.com>
2022-04-28 13:12:42 +0200

    Feat: simplified event loop

--- generics/event/loop.go
+++ generics/event/loop.go
@@ -1,33 +1,14 @@
 package event
 
 import (
-	"sync"
-
 	"github.com/iotaledger/hive.go/workerpool"
 )
 
 const loopQueueSize = 1000
 
-type EventLoop struct {
-	once sync.Once
+var Loop *workerpool.BlockingQueuedWorkerPool
 
-	wp *workerpool.BlockingQueuedWorkerPool
-}
-
-var Loop EventLoop
-
-func (l *EventLoop) GetWorkerPool() *workerpool.BlockingQueuedWorkerPool {
-	l.once.Do(func() {
-		l.wp = workerpool.NewBlockingQueuedWorkerPool(workerpool.QueueSize(loopQueueSize), workerpool.FlushTasksAtShutdown(true))
-		l.wp.Start()
-	})
-	return l.wp
-}
-
-func (l *EventLoop) Submit(f func()) {
-	l.GetWorkerPool().Submit(f)
-}
-
-func (l *EventLoop) TrySubmit(f func()) (added bool) {
-	return l.GetWorkerPool().TrySubmit(f)
+func init() {
+	Loop = workerpool.NewBlockingQueuedWorkerPool(workerpool.QueueSize(loopQueueSize), workerpool.FlushTasksAtShutdown(true))
+	Loop.Start()
 }
