46add8a04d749621200ef86d06b4fadd8bc417a9
Piotr Macek <piotr.macek@iota.org>
2022-05-04 12:29:46 +0200

    Generic set implements serix serialization.

--- generics/set/generic_set.go
+++ generics/set/generic_set.go
@@ -1,7 +1,13 @@
 package set
 
 import (
+	"context"
+
+	"github.com/cockroachdb/errors"
+
 	"github.com/iotaledger/hive.go/datastructure/set"
+	"github.com/iotaledger/hive.go/serializer/v2"
+	"github.com/iotaledger/hive.go/serix"
 )
 
 // type genericSet[T comparable] struct { implements a generic wrapper for a non-generic Set.
@@ -36,6 +42,48 @@ func (set *genericSet[T]) ForEach(callback func(element T)) {
 	set.Set.ForEach(func(element interface{}) {
 		callback(element.(T))
 	})
+}
+
+// Encode returns a serialized byte slice of the object.
+func (set *genericSet[T]) Encode() ([]byte, error) {
+	seri := serializer.NewSerializer()
+
+	seri.WriteNum(uint32(set.Size()), func(err error) error {
+		return errors.Wrap(err, "failed to write set size to serializer")
+	})
+
+	set.ForEach(func(elem T) {
+		bytes, err := serix.DefaultAPI.Encode(context.Background(), elem)
+		if err != nil {
+			seri.AbortIf(func(err error) error {
+				return errors.Wrap(err, "failed to serialize element of a set")
+			})
+		}
+		seri.WriteBytes(bytes, func(err error) error {
+			return errors.Wrap(err, "failed to write elem to serializer")
+		})
+	})
+	return seri.Serialize()
+}
+
+// Decode deserializes bytes into a valid object.
+func (set *genericSet[T]) Decode(b []byte) (bytesRead int, err error) {
+	var elemCount uint32
+	bytesRead, err = serix.DefaultAPI.Decode(context.Background(), b, &elemCount)
+	if err != nil {
+		return 0, err
+	}
+
+	for i := uint32(0); i < elemCount; i++ {
+		var elem T
+		bytesReadVoter, err := serix.DefaultAPI.Decode(context.Background(), b[bytesRead:], &elem)
+		if err != nil {
+			return 0, err
+		}
+		bytesRead += bytesReadVoter
+		set.Set.Add(elem)
+	}
+	return bytesRead, nil
 }
 
 // code contract - make sure the type implements the interface
