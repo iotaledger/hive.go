0e2e8d0030288292628eb59725c529089299abd0
jonastheis <4181434+jonastheis@users.noreply.github.com>
2022-05-26 20:14:24 +0100

    Remove decode and encode functions for models

--- generics/model/model.go
+++ generics/model/model.go
@@ -13,17 +13,17 @@ import (
 	"github.com/iotaledger/hive.go/serix"
 )
 
-type Model[IDType comparable, ModelType any] struct {
+type Model[IDType, ModelType any] struct {
 	id      *IDType
 	idFunc  func(model *ModelType) IDType
 	idMutex sync.RWMutex
-	M       ModelType
+	M       ModelType `serix:"0"`
 
 	sync.RWMutex
 	objectstorage.StorableObjectFlags
 }
 
-func NewModel[IDType comparable, ModelType any](model ModelType, optionalIDFunc ...func(model *ModelType) IDType) (new Model[IDType, ModelType]) {
+func NewModel[IDType, ModelType any](model ModelType, optionalIDFunc ...func(model *ModelType) IDType) (new Model[IDType, ModelType]) {
 	if len(optionalIDFunc) == 0 {
 		optionalIDFunc = append(optionalIDFunc, func(model *ModelType) (key IDType) {
 			return key
@@ -110,7 +110,7 @@ func (m *Model[IDType, ModelType]) String() string {
 
 // region ReferenceModel ///////////////////////////////////////////////////////////////////////////////////////////////
 
-type ReferenceModel[SourceIDType, TargetIDType comparable] struct {
+type ReferenceModel[SourceIDType, TargetIDType any] struct {
 	SourceID SourceIDType
 	TargetID TargetIDType
 
@@ -118,7 +118,7 @@ type ReferenceModel[SourceIDType, TargetIDType comparable] struct {
 	objectstorage.StorableObjectFlags
 }
 
-func NewReferenceModel[SourceIDType, TargetIDType comparable](s SourceIDType, t TargetIDType) (new ReferenceModel[SourceIDType, TargetIDType]) {
+func NewReferenceModel[SourceIDType, TargetIDType any](s SourceIDType, t TargetIDType) (new ReferenceModel[SourceIDType, TargetIDType]) {
 	new = ReferenceModel[SourceIDType, TargetIDType]{
 		SourceID: s,
 		TargetID: t,
@@ -129,51 +129,37 @@ func NewReferenceModel[SourceIDType, TargetIDType comparable](s SourceIDType, t 
 	return new
 }
 
-func (m *ReferenceModel[SourceIDType, TargetIDType]) Decode(b []byte) (int, error) {
+func (m *ReferenceModel[SourceIDType, TargetIDType]) FromObjectStorage(key, _ []byte) (err error) {
 	m.Lock()
 	defer m.Unlock()
 
-	consumedBytesSource, err := serix.DefaultAPI.Decode(context.Background(), b, &m.SourceID, serix.WithValidation())
+	consumedBytesSource, err := serix.DefaultAPI.Decode(context.Background(), key, &m.SourceID, serix.WithValidation())
 	if err != nil {
-		return consumedBytesSource, errors.Errorf("failed to decode SourceID: %w", err)
+		return errors.Errorf("failed to decode SourceID: %w", err)
 	}
 
-	consumedBytesTarget, err := serix.DefaultAPI.Decode(context.Background(), b[consumedBytesSource:], &m.TargetID, serix.WithValidation())
+	_, err = serix.DefaultAPI.Decode(context.Background(), key[consumedBytesSource:], &m.TargetID, serix.WithValidation())
 	if err != nil {
-		return consumedBytesTarget, errors.Errorf("failed to decode TargetID: %w", err)
+		return errors.Errorf("failed to decode TargetID: %w", err)
 	}
 
-	return consumedBytesSource + consumedBytesTarget, nil
+	return err
 }
 
-func (m *ReferenceModel[SourceIDType, TargetIDType]) Encode() ([]byte, error) {
+func (m *ReferenceModel[SourceIDType, TargetIDType]) ObjectStorageKey() (key []byte) {
 	m.RLock()
 	defer m.RUnlock()
 
 	sourceBytes, err := serix.DefaultAPI.Encode(context.Background(), &m.SourceID, serix.WithValidation())
 	if err != nil {
-		return nil, errors.Errorf("failed to encode source: %w", err)
+		panic(errors.Errorf("failed to encode source: %w", err))
 	}
 	targetBytes, err := serix.DefaultAPI.Encode(context.Background(), &m.TargetID, serix.WithValidation())
 	if err != nil {
-		return nil, errors.Errorf("failed to encode target: %w", err)
+		panic(errors.Errorf("failed to encode target: %w", err))
 	}
 
-	return byteutils.ConcatBytes(sourceBytes, targetBytes), nil
-}
-
-func (m *ReferenceModel[SourceIDType, TargetIDType]) FromObjectStorage(key, _ []byte) (err error) {
-	_, err = m.Decode(key)
-	return err
-}
-
-func (m *ReferenceModel[SourceIDType, TargetIDType]) ObjectStorageKey() (key []byte) {
-	key, err := m.Encode()
-	if err != nil {
-		panic(err)
-	}
-
-	return key
+	return byteutils.ConcatBytes(sourceBytes, targetBytes)
 }
 
 func (m *ReferenceModel[SourceIDType, TargetIDType]) ObjectStorageValue() (value []byte) {
@@ -196,7 +182,7 @@ func (m *ReferenceModel[SourceIDType, TargetIDType]) String() string {
 
 // region ReferenceModelWithMetadata ///////////////////////////////////////////////////////////////////////////////////
 
-type ReferenceModelWithMetadata[SourceIDType, TargetIDType comparable, ModelType any] struct {
+type ReferenceModelWithMetadata[SourceIDType, TargetIDType, ModelType any] struct {
 	SourceID SourceIDType
 	TargetID TargetIDType
 	M        ModelType
@@ -205,7 +191,7 @@ type ReferenceModelWithMetadata[SourceIDType, TargetIDType comparable, ModelType
 	objectstorage.StorableObjectFlags
 }
 
-func NewReferenceModelWithMetadata[SourceIDType, TargetIDType comparable, ModelType any](s SourceIDType, t TargetIDType, m ModelType) (new ReferenceModelWithMetadata[SourceIDType, TargetIDType, ModelType]) {
+func NewReferenceModelWithMetadata[SourceIDType, TargetIDType, ModelType any](s SourceIDType, t TargetIDType, m ModelType) (new ReferenceModelWithMetadata[SourceIDType, TargetIDType, ModelType]) {
 	new = ReferenceModelWithMetadata[SourceIDType, TargetIDType, ModelType]{
 		SourceID: s,
 		TargetID: t,
@@ -215,20 +201,6 @@ func NewReferenceModelWithMetadata[SourceIDType, TargetIDType comparable, ModelT
 	new.Persist()
 
 	return new
-}
-
-func (m *ReferenceModelWithMetadata[SourceIDType, TargetIDType, ModelType]) Decode(b []byte) (int, error) {
-	m.Lock()
-	defer m.Unlock()
-
-	return serix.DefaultAPI.Decode(context.Background(), b, &m.M)
-}
-
-func (m *ReferenceModelWithMetadata[SourceIDType, TargetIDType, ModelType]) Encode() ([]byte, error) {
-	m.RLock()
-	defer m.RUnlock()
-
-	return serix.DefaultAPI.Encode(context.Background(), &m.M, serix.WithValidation())
 }
 
 func (m *ReferenceModelWithMetadata[SourceIDType, TargetIDType, ModelType]) FromObjectStorage(key, data []byte) (err error) {
