49535b2e12d9d567aad2d7517209749cc93e8a97
Hans Moog <3293976+hmoog@users.noreply.github.com>
2022-06-18 01:41:48 +0200

    Feat: added PrecisionTicker to timeutils

--- generics/priorityqueue/priorityqueue.go
+++ generics/priorityqueue/priorityqueue.go
@@ -2,7 +2,6 @@ package priorityqueue
 
 import (
 	"container/heap"
-	"sync"
 
 	"github.com/iotaledger/hive.go/generics/constraints"
 )
@@ -10,8 +9,6 @@ import (
 // PriorityQueue is a heap based priority queue.
 type PriorityQueue[T constraints.Comparable[T]] struct {
 	heap queue[T]
-
-	sync.RWMutex
 }
 
 // New creates a new PriorityQueue.
@@ -23,32 +20,30 @@ func New[T constraints.Comparable[T]]() *PriorityQueue[T] {
 
 // Push adds x to the PriorityQueue.
 func (p *PriorityQueue[T]) Push(x T) {
-	p.Lock()
-	defer p.Unlock()
-
 	heap.Push(&p.heap, x)
 }
 
 // Pop removes and returns the element with the highest priority.
 func (p *PriorityQueue[T]) Pop() T {
-	p.Lock()
-	defer p.Unlock()
-
 	return *heap.Pop(&p.heap).(*T)
+}
+
+// PopIndex removes and returns the element at the given index.
+func (p *PriorityQueue[T]) PopIndex(index int) int {
+	return *heap.Remove(&p.heap, index).(*int)
 }
 
 // Peek returns the element with the highest priority without removing it.
 func (p *PriorityQueue[T]) Peek() T {
-	p.RLock()
-	defer p.RUnlock()
-
 	return *p.heap[0]
+}
+
+// PeekIndex returns the element at the given index without removing it.
+func (p *PriorityQueue[T]) PeekIndex(index int) T {
+	return *p.heap[index]
 }
 
 // Size returns the number of elements in the PriorityQueue.
 func (p *PriorityQueue[T]) Size() int {
-	p.RLock()
-	defer p.RUnlock()
-
 	return p.heap.Len()
 }
