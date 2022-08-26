29e903b039a6fdb5c1663d1a40198386e796e66c
jonastheis <4181434+jonastheis@users.noreply.github.com>
2022-08-26 09:44:40 +0100

    Add comments, wrap errors and clean up

--- core/generics/kvstore/typedstore.go
+++ core/generics/kvstore/typedstore.go
@@ -1,28 +1,40 @@
 package kvstore
 
 import (
+	"github.com/pkg/errors"
+
 	"github.com/iotaledger/hive.go/core/kvstore"
 	"github.com/iotaledger/hive.go/core/serix"
 )
 
+// DeAndSerializable is an interface for a type that is Serializable and Deserializable.
 type DeAndSerializable interface {
 	serix.Serializable
 	serix.Deserializable
 }
 
-type TypedStored[K, V DeAndSerializable] struct {
+// TypedStore is a generically typed wrapper around a kvstore.KVStore that abstracts serialization away.
+type TypedStore[K, V DeAndSerializable] struct {
 	kv kvstore.KVStore
 }
 
-func (t *TypedStored[K, V]) Get(key K) (value V, exists bool, err error) {
+// NewTypedStore is the constructor for TypedStore.
+func NewTypedStore[K, V DeAndSerializable](kv kvstore.KVStore) *TypedStore[K, V] {
+	return &TypedStore[K, V]{
+		kv: kv,
+	}
+}
+
+// Get gets the given key or an error if an error occurred.
+func (t *TypedStore[K, V]) Get(key K) (value V, exists bool, err error) {
 	keyBytes, err := key.Encode()
 	if err != nil {
-		return nil, false, err
+		return nil, false, errors.Wrap(err, "failed to encode key")
 	}
 
 	valueBytes, err := t.kv.Get(keyBytes)
 	if err != nil {
-		return nil, false, err
+		return nil, false, errors.Wrap(err, "failed to retrieve from KV store")
 	}
 	if valueBytes == nil {
 		return nil, false, nil
@@ -30,20 +42,28 @@ func (t *TypedStored[K, V]) Get(key K) (value V, exists bool, err error) {
 
 	_, err = value.Decode(valueBytes)
 	if err != nil {
-		return nil, false, err
+		return nil, false, errors.Wrap(err, "failed to decode value")
 	}
+
 	return value, true, nil
 }
 
-func (t *TypedStored[K, V]) Set(key K, value V) (err error) {
+// Set sets the given key and value.
+func (t *TypedStore[K, V]) Set(key K, value V) (err error) {
 	keyBytes, err := key.Encode()
 	if err != nil {
-		return err
+		return errors.Wrap(err, "failed to encode key")
 	}
+
 	valueBytes, err := value.Encode()
 	if err != nil {
-		return err
+		return errors.Wrap(err, "failed to encode value")
 	}
+
 	err = t.kv.Set(keyBytes, valueBytes)
-	return err
+	if err != nil {
+		return errors.Wrap(err, "failed to store in KV store")
+	}
+
+	return nil
 }
