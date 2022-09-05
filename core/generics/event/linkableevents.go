099cf85232d5735c094c4635b101b0db6e0b99db
Hans Moog <3293976+hmoog@users.noreply.github.com>
2022-09-05 17:27:58 +0200

    Feat: Linkable Event Dictionaries (for dispatching public API endpoints) (#394)
    * Feat: Added Linkable Events
    
    * Feat: finished refactor
    
    * Refactor: removed remains in old files
    
    * Refactor: refactored comments
    
    * Refactor: refactored code
    
    * Fix: fixed indentation
    
    * Fix: fixed last thing
    
    * Refactor: refactored code
    
    * Refactor: refactored more code
    
    * Feat: final version
    
    * Refactor: renamed constructor
    
    * Fix: fixed bug in code
    
    * Feat: simplified API and allowed to UnLink
    
    * Update core/generics/event/linkableevents.go
    
    Co-authored-by: Jonas Theis <4181434+jonastheis@users.noreply.github.com>
    
    Co-authored-by: Jonas Theis <4181434+jonastheis@users.noreply.github.com>

---
+++ core/generics/event/linkableevents.go
@@ -0,0 +1,139 @@
+package event
+
+import (
+	"reflect"
+	"sync"
+)
+
+// region Linkable /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Linkable represents a special kind of Event that is part of a LinkableCollection of events.
+type Linkable[A any, B any, C ptrLinkableCollectionType[B, C]] struct {
+	linkedEvent *Linkable[A, B, C]
+	linkClosure *Closure[A]
+
+	*Event[A]
+}
+
+// NewLinkable creates a new Linkable.
+func NewLinkable[A any, B any, C ptrLinkableCollectionType[B, C]]() (newEvent *Linkable[A, B, C]) {
+	return &Linkable[A, B, C]{
+		Event: New[A](),
+	}
+}
+
+// LinkTo links the Linkable to the given Linkable.
+func (e *Linkable[A, B, C]) LinkTo(optLinkTargets ...*Linkable[A, B, C]) {
+	if len(optLinkTargets) == 0 || e.linkedEvent == optLinkTargets[0] || e == optLinkTargets[0] {
+		return
+	}
+
+	if e.linkClosure != nil {
+		e.linkedEvent.Detach(e.linkClosure)
+	}
+
+	if e.linkedEvent = optLinkTargets[0]; e.linkedEvent == nil {
+		e.linkClosure = nil
+		return
+	}
+
+	if e.linkClosure == nil {
+		e.linkClosure = NewClosure(e.Trigger)
+	}
+
+	e.linkedEvent.Hook(e.linkClosure)
+}
+
+// endregion ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// region LinkableCollection ///////////////////////////////////////////////////////////////////////////////////////////
+
+// LinkableCollection can be embedded into collections of linkable Events to make the entire collection linkable.
+type LinkableCollection[A any, B ptrLinkableCollectionType[A, B]] struct {
+	linkUpdated *Event[B]
+	sync.Once
+}
+
+// LinkTo offers a way to link the collection to another collection of the same type.
+func (l *LinkableCollection[A, B]) LinkTo(optLinkTargets ...B) {
+	if len(optLinkTargets) == 0 {
+		return
+	}
+
+	l.linkUpdatedEvent().Trigger(optLinkTargets[0])
+}
+
+// onLinkUpdated registers a callback to be called when the link to the referenced LinkableCollection is set or updated.
+func (l *LinkableCollection[A, B]) onLinkUpdated(callback func(linkTarget B)) {
+	l.linkUpdatedEvent().Hook(NewClosure(callback))
+}
+
+// linkUpdatedEvent returns the linkUpdated Event.
+func (l *LinkableCollection[A, B]) linkUpdatedEvent() (linkUpdatedEvent *Event[B]) {
+	l.Do(func() {
+		l.linkUpdated = New[B]()
+	})
+
+	return l.linkUpdated
+}
+
+// endregion ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// region LinkableConstructor ////////////////////////////////////////////////////////////////////////////////////////////
+
+// LinkableConstructor returns the linkable constructor for the given type.
+func LinkableConstructor[A any, B ptrLinkableCollectionType[A, B]](newFunc func() B) (constructor func(...B) B) {
+	return func(optLinkTargets ...B) (self B) {
+		self = newFunc()
+
+		selfValue := reflect.ValueOf(self).Elem()
+		self.onLinkUpdated(func(linkTarget B) {
+			if linkTarget == nil {
+				linkTarget = new(A)
+			}
+
+			linkTargetValue := reflect.ValueOf(linkTarget).Elem()
+
+			for i := 0; i < selfValue.NumField(); i++ {
+				if sourceField := selfValue.Field(i); sourceField.Kind() == reflect.Ptr {
+					if linkTo := sourceField.MethodByName("LinkTo"); linkTo.IsValid() {
+						linkTo.Call([]reflect.Value{linkTargetValue.Field(i)})
+					}
+				}
+			}
+		})
+		self.LinkTo(optLinkTargets...)
+
+		return self
+	}
+}
+
+// endregion ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// region types and interfaces /////////////////////////////////////////////////////////////////////////////////////////
+
+// linkableType is the interface for linkable elements.
+type linkableType[B any] interface {
+	LinkTo(optLinkTargets ...B)
+}
+
+// linkableCollectionType is the interface for a LinkableCollection.
+type linkableCollectionType[B any] interface {
+	onLinkUpdated(callback func(B))
+
+	linkableType[B]
+}
+
+// ptrType is a helper type to create a pointer type.
+type ptrType[A any] interface {
+	*A
+}
+
+// ptrLinkableCollectionType is a helper type to create a pointer to a linkableCollectionType.
+type ptrLinkableCollectionType[A any, B ptrType[A]] interface {
+	*A
+
+	linkableCollectionType[B]
+}
+
+// endregion ///////////////////////////////////////////////////////////////////////////////////////////////////////////
---
+++ core/generics/event/linkableevents_test.go
@@ -0,0 +1,158 @@
+package event_test
+
+import (
+	"sync/atomic"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/iotaledger/hive.go/core/generics/event"
+)
+
+// region Tests ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// TestLinkableEvents tests if switching between linked sources correctly works.
+func TestLinkableEvents(t *testing.T) {
+	// create source events
+	internalEvents1 := NewEvents()
+	internalEvents2 := NewEvents()
+	publicEvents := NewEvents(internalEvents1)
+
+	// create trigger counters
+	var internal1TriggeredCount, internal1NestedTriggeredCount, internal2TriggeredCount, internal2NestedTriggeredCount,
+		publicTriggerCount, publicNestedTriggerCount uint64
+
+	// register triggers
+	internalEvents1.BlockMissing.Hook(event.NewClosure(func(int) { atomic.AddUint64(&internal1TriggeredCount, 1) }))
+	internalEvents1.NestedEvents.BlockReceived.Hook(event.NewClosure(func(bool) { atomic.AddUint64(&internal1NestedTriggeredCount, 1) }))
+	internalEvents2.BlockMissing.Hook(event.NewClosure(func(int) { atomic.AddUint64(&internal2TriggeredCount, 1) }))
+	internalEvents2.NestedEvents.BlockReceived.Hook(event.NewClosure(func(bool) { atomic.AddUint64(&internal2NestedTriggeredCount, 1) }))
+	publicEvents.BlockMissing.Hook(event.NewClosure[int](func(event int) { atomic.AddUint64(&publicTriggerCount, 1) }))
+	publicEvents.NestedEvents.BlockReceived.Hook(event.NewClosure(func(isBlockReceived bool) { atomic.AddUint64(&publicNestedTriggerCount, 1) }))
+
+	// trigger on both source events (after linked through initialization)
+	internalEvents1.BlockMissing.Trigger(4)
+	internalEvents2.BlockMissing.Trigger(5)
+	internalEvents1.NestedEvents.BlockReceived.Trigger(true)
+	internalEvents2.NestedEvents.BlockReceived.Trigger(true)
+	require.EqualValues(t, 1, internal1TriggeredCount)
+	require.EqualValues(t, 1, internal1NestedTriggeredCount)
+	require.EqualValues(t, 1, internal2TriggeredCount)
+	require.EqualValues(t, 1, internal2NestedTriggeredCount)
+	require.EqualValues(t, 1, publicTriggerCount)
+	require.EqualValues(t, 1, publicNestedTriggerCount)
+
+	publicEvents.LinkTo(internalEvents2)
+
+	// trigger on chosen source event
+	internalEvents2.BlockMissing.Trigger(1)
+	internalEvents2.BlockMissing.Trigger(2)
+	internalEvents2.NestedEvents.BlockReceived.Trigger(true)
+	internalEvents2.NestedEvents.BlockReceived.Trigger(true)
+	require.EqualValues(t, 1, internal1TriggeredCount)
+	require.EqualValues(t, 1, internal1NestedTriggeredCount)
+	require.EqualValues(t, 3, internal2TriggeredCount)
+	require.EqualValues(t, 3, internal2NestedTriggeredCount)
+	require.EqualValues(t, 3, publicTriggerCount)
+	require.EqualValues(t, 3, publicNestedTriggerCount)
+
+	publicEvents.LinkTo(internalEvents1)
+
+	// trigger on other source event
+	internalEvents2.BlockMissing.Trigger(2)
+	internalEvents2.BlockMissing.Trigger(7)
+	internalEvents2.NestedEvents.BlockReceived.Trigger(true)
+	internalEvents2.NestedEvents.BlockReceived.Trigger(true)
+	require.EqualValues(t, 1, internal1TriggeredCount)
+	require.EqualValues(t, 1, internal1NestedTriggeredCount)
+	require.EqualValues(t, 5, internal2TriggeredCount)
+	require.EqualValues(t, 5, internal2NestedTriggeredCount)
+	require.EqualValues(t, 3, publicTriggerCount)
+	require.EqualValues(t, 3, publicNestedTriggerCount)
+
+	// trigger on both source events (after linked through LinkTo)
+	internalEvents1.BlockMissing.Trigger(4)
+	internalEvents2.BlockMissing.Trigger(5)
+	internalEvents1.NestedEvents.BlockReceived.Trigger(true)
+	internalEvents2.NestedEvents.BlockReceived.Trigger(true)
+	require.EqualValues(t, 2, internal1TriggeredCount)
+	require.EqualValues(t, 2, internal1NestedTriggeredCount)
+	require.EqualValues(t, 6, internal2TriggeredCount)
+	require.EqualValues(t, 6, internal2NestedTriggeredCount)
+	require.EqualValues(t, 4, publicTriggerCount)
+	require.EqualValues(t, 4, publicNestedTriggerCount)
+
+	publicEvents.LinkTo(nil)
+
+	// trigger on both source events (after linked through LinkTo)
+	internalEvents1.BlockMissing.Trigger(4)
+	internalEvents2.BlockMissing.Trigger(5)
+	internalEvents1.NestedEvents.BlockReceived.Trigger(true)
+	internalEvents2.NestedEvents.BlockReceived.Trigger(true)
+	require.EqualValues(t, 3, internal1TriggeredCount)
+	require.EqualValues(t, 3, internal1NestedTriggeredCount)
+	require.EqualValues(t, 7, internal2TriggeredCount)
+	require.EqualValues(t, 7, internal2NestedTriggeredCount)
+	require.EqualValues(t, 4, publicTriggerCount)
+	require.EqualValues(t, 4, publicNestedTriggerCount)
+
+	publicEvents.LinkTo(internalEvents1)
+
+	// trigger on both source events (after linked through LinkTo)
+	internalEvents1.BlockMissing.Trigger(4)
+	internalEvents2.BlockMissing.Trigger(5)
+	internalEvents1.NestedEvents.BlockReceived.Trigger(true)
+	internalEvents2.NestedEvents.BlockReceived.Trigger(true)
+	require.EqualValues(t, 4, internal1TriggeredCount)
+	require.EqualValues(t, 4, internal1NestedTriggeredCount)
+	require.EqualValues(t, 8, internal2TriggeredCount)
+	require.EqualValues(t, 8, internal2NestedTriggeredCount)
+	require.EqualValues(t, 5, publicTriggerCount)
+	require.EqualValues(t, 5, publicNestedTriggerCount)
+}
+
+// endregion ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// region Events ///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Events is an example of a set of events that can be linked to other Events.
+type Events struct {
+	// BlockMissing is triggered when a block is missing.
+	BlockMissing *event.Linkable[int, Events, *Events]
+
+	// NestedEvents is a nested set of events that can be linked to other Events.
+	NestedEvents *NestedEvents
+
+	// LinkableCollection imports the logic to make the struct linkable.
+	event.LinkableCollection[Events, *Events]
+}
+
+// NewEvents is the constructor of the Events object (it is generated by a generic factory).
+var NewEvents = event.LinkableConstructor(func() (newInstance *Events) {
+	return &Events{
+		BlockMissing: event.NewLinkable[int, Events](),
+		NestedEvents: NewNestedEvents(),
+	}
+})
+
+// endregion ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// region NestedEvents /////////////////////////////////////////////////////////////////////////////////////////////////
+
+// NewEvents is a nested set of Events that is embedded in the Events object.
+type NestedEvents struct {
+	// BlockReceived is triggered when a block is received.
+	BlockReceived *event.Linkable[bool, NestedEvents, *NestedEvents]
+
+	// LinkableCollection imports the logic to make the struct linkable.
+	event.LinkableCollection[NestedEvents, *NestedEvents]
+}
+
+// NewNestedEvents is the constructor of the NestedEvents object (it is generated by a generic factory).
+var NewNestedEvents = event.LinkableConstructor(func() (newInstance *NestedEvents) {
+	return &NestedEvents{
+		BlockReceived: event.NewLinkable[bool, NestedEvents](),
+	}
+})
+
+// endregion ///////////////////////////////////////////////////////////////////////////////////////////////////////////
