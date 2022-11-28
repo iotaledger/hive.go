1f3eedeecc7176424c0c892b480bb7534606ba9b
Alexander Sporn <github@alexsporn.de>
2022-11-28 13:45:19 +0100

    properly fail the CI tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-find . -name go.mod -execdir go test ./... \;
+find . -name go.mod -print0 |  xargs -0 -n1 dirname | xargs -t -n1 -I {} bash -c 'set -euxo pipefail && cd "{}" && go test ./...'
