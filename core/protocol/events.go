81f61c4e8b7de5f085b37a817b4c36540ed28827
Hans Moog <3293976+hmoog@users.noreply.github.com>
2023-02-15 16:11:53 +0100

    Fix: fixed refactor bug

--- core/protocol/events.go
+++ core/protocol/events.go
@@ -1,16 +1,16 @@
 package protocol
 
 import (
-	"github.com/iotaledger/hive.go/core/generics/event"
+	"github.com/iotaledger/hive.go/runtime/event"
 )
 
 // Events holds protocol related events.
 type Events struct {
 	// Holds event instances to attach to for received messages.
 	// Use a message's ID to get the corresponding event.
-	Received []*event.Event[[]byte]
+	Received []*event.Event1[[]byte]
 	// Fired for generic protocol errors.
-	Error *event.Event[error]
+	Error *event.Event1[error]
 }
 
 type VoidEvent struct{}
