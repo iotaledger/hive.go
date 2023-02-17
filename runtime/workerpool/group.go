7ad1d65200f6dc61cf6e0763b443667b0e6b98ad
Hans Moog <3293976+hmoog@users.noreply.github.com>
2023-02-17 02:55:53 +0100

    Feat: cleaned up workerpool package

--- runtime/workerpool/group.go
+++ runtime/workerpool/group.go
@@ -12,36 +12,40 @@ import (
 
 // Group is a group of WorkerPools that can be managed as a whole.
 type Group struct {
+	// PendingChildrenCounter contains the number of children that are currently pending.
 	PendingChildrenCounter *syncutils.Counter
 
-	name   string
-	pools  *orderedmap.OrderedMap[string, *WorkerPool]
+	// name is the name of the Group.
+	name string
+
+	// pools is a map of WorkerPools that are managed by this Group.
+	pools *orderedmap.OrderedMap[string, *WorkerPool]
+
+	// groups is a map of subgroups that are managed by this Group.
 	groups *orderedmap.OrderedMap[string, *Group]
-	root   *Group
+
+	// root is the root Group of this Group.
+	root *Group
 }
 
 // NewGroup creates a new Group.
-func NewGroup(name string) (group *Group) {
-	return newGroupWithRoot(name, nil)
-}
-
-// newGroupWithRoot creates a new Group with a root Group.
-func newGroupWithRoot(name string, root *Group) (group *Group) {
+func NewGroup(name string) *Group {
 	return &Group{
 		PendingChildrenCounter: syncutils.NewCounter(),
 		name:                   name,
 		pools:                  orderedmap.New[string, *WorkerPool](),
 		groups:                 orderedmap.New[string, *Group](),
-		root:                   root,
 	}
 }
 
+// Name returns the name of the Group.
 func (g *Group) Name() (name string) {
 	return g.name
 }
 
-func (g *Group) CreatePool(name string, optsWorkerCount ...int) (pool *WorkerPool) {
-	pool = New(name, optsWorkerCount...)
+// CreatePool creates a new WorkerPool with the given name and returns it.
+func (g *Group) CreatePool(name string, optWorkerCount ...int) *WorkerPool {
+	pool := New(name, optWorkerCount...)
 	pool.PendingTasksCounter.Subscribe(func(oldValue, newValue int) {
 		if oldValue == 0 {
 			g.PendingChildrenCounter.Increase()
@@ -57,24 +61,15 @@ func (g *Group) CreatePool(name string, optsWorkerCount ...int) (pool *WorkerPoo
 	return pool.Start()
 }
 
-func (g *Group) Root() *Group {
-	return lo.Cond(g.root != nil, g.root, g)
-}
-
-func (g *Group) Wait() {
-	g.PendingChildrenCounter.WaitIsZero()
-}
-
-func (g *Group) WaitAll() {
-	g.Root().Wait()
-}
-
+// Pool returns the WorkerPool with the given name.
 func (g *Group) Pool(name string) (pool *WorkerPool, exists bool) {
 	return g.pools.Get(name)
 }
 
-func (g *Group) Pools() (pools map[string]*WorkerPool) {
-	pools = make(map[string]*WorkerPool)
+// Pools returns all WorkerPools of the Group.
+func (g *Group) Pools() map[string]*WorkerPool {
+	pools := make(map[string]*WorkerPool)
+
 	g.pools.ForEach(func(name string, pool *WorkerPool) bool {
 		pools[fmt.Sprintf("%s.%s", g.name, name)] = pool
 		return true
@@ -90,8 +85,10 @@ func (g *Group) Pools() (pools map[string]*WorkerPool) {
 	return pools
 }
 
-func (g *Group) CreateGroup(name string) (group *Group) {
-	group = newGroupWithRoot(name, g.Root())
+// CreateGroup creates a new Group with the given name and returns it.
+func (g *Group) CreateGroup(name string) *Group {
+	group := NewGroup(name)
+	group.root = g.Root()
 	group.PendingChildrenCounter.Subscribe(func(oldValue, newValue int) {
 		if oldValue == 0 {
 			g.PendingChildrenCounter.Increase()
@@ -107,68 +104,101 @@ func (g *Group) CreateGroup(name string) (group *Group) {
 	return group
 }
 
+// Group returns the Group with the given name.
 func (g *Group) Group(name string) (pool *Group, exists bool) {
 	return g.groups.Get(name)
 }
 
+// WaitChildren waits until all children of the Group are idle.
+func (g *Group) WaitChildren() {
+	g.PendingChildrenCounter.WaitIsZero()
+}
+
+// WaitParents waits until all parents of the Group are idle.
+func (g *Group) WaitParents() {
+	g.Root().WaitChildren()
+}
+
+// Root returns the root Group of the Group.
+func (g *Group) Root() *Group {
+	return lo.Cond(g.root != nil, g.root, g)
+}
+
+// Shutdown shuts down all child elements of the Group.
 func (g *Group) Shutdown() {
 	g.PendingChildrenCounter.WaitIsZero()
 
 	g.shutdown()
 }
 
-func (g *Group) String() (humanReadable string) {
-	if indentedString := g.indentedString(0); indentedString != "" {
-		return strings.TrimRight(g.indentedString(0), "\r\n")
+// String returns a human-readable string representation of the Group.
+func (g *Group) String() string {
+	if indentedString := g.string(0); indentedString != "" {
+		return strings.TrimRight(g.string(0), "\r\n")
 	}
 
 	return "> " + g.name + " (0 pending children)"
 }
 
-func (g *Group) indentedString(indentation int) (humanReadable string) {
-	if pendingChildren := g.PendingChildrenCounter.Get(); pendingChildren != 0 {
-		if children := g.childrenString(indentation + 1); children != "" {
-			humanReadable = strings.Repeat(indentationString, indentation) + "> " + g.name + " (" + strconv.Itoa(pendingChildren) + " pending children) {\n"
-			humanReadable += children
-			humanReadable += strings.Repeat(indentationString, indentation) + "}\n"
-		}
+// string returns a human-readable representation of the Group with the given indentation.
+func (g *Group) string(indent int) string {
+	pendingChildCounter := g.PendingChildrenCounter.Get()
+	if pendingChildCounter == 0 {
+		return ""
 	}
 
-	return humanReadable
-}
-
-func (g *Group) childrenString(indentation int) (humanReadable string) {
-	humanReadable = g.poolsString(indentation)
-
-	groups := g.groupsString(indentation)
-	if humanReadable != "" && groups != "" {
-		humanReadable += strings.Repeat(indentationString, indentation) + "\n"
+	children := g.childrenString(indent + 1)
+	if children == "" {
+		return ""
 	}
 
-	return humanReadable + groups
+	result := strings.Repeat(indentStr, indent) + "> " + g.name + " (" + strconv.Itoa(pendingChildCounter) + " pending children) {\n"
+	result += children
+	result += strings.Repeat(indentStr, indent) + "}\n"
+
+	return result
 }
 
-func (g *Group) poolsString(indentation int) (humanReadable string) {
+// childrenString returns a human-readable representation of the children of the Group with the given indentation.
+func (g *Group) childrenString(indent int) string {
+	pools := g.poolsString(indent)
+	groups := g.groupsString(indent)
+
+	if groups == "" {
+		return pools
+	}
+	if pools == "" {
+		return groups
+	}
+
+	return pools + strings.Repeat(indentStr, indent) + "\n" + groups
+}
+
+// poolsString returns a human-readable representation of the WorkerPools of the Group with the given indentation.
+func (g *Group) poolsString(indent int) string {
+	result := ""
 	g.pools.ForEach(func(key string, value *WorkerPool) bool {
 		if currentValue := value.PendingTasksCounter.Get(); currentValue > 0 {
-			humanReadable += strings.Repeat(indentationString, indentation) + "- " + key + " (" + strconv.Itoa(currentValue) + " pending tasks)\n"
+			result += strings.Repeat(indentStr, indent) + "- " + key + " (" + strconv.Itoa(currentValue) + " pending tasks)\n"
 		}
-
 		return true
 	})
 
-	return humanReadable
+	return result
 }
 
-func (g *Group) groupsString(indentation int) (humanReadable string) {
+// groupsString returns a human-readable representation of the Groups of the Group with the given indentation.
+func (g *Group) groupsString(indent int) string {
+	result := ""
 	g.groups.ForEach(func(key string, value *Group) bool {
-		humanReadable += value.indentedString(indentation)
+		result += value.string(indent)
 		return true
 	})
 
-	return humanReadable
+	return result
 }
 
+// shutdown shuts down all child elements of the Group.
 func (g *Group) shutdown() {
 	g.pools.ForEach(func(_ string, pool *WorkerPool) bool {
 		pool.Shutdown(true)
@@ -181,4 +211,5 @@ func (g *Group) shutdown() {
 	})
 }
 
-const indentationString = "    "
+// indentStr is the string used for indentation.
+const indentStr = "    "
--- runtime/workerpool/workerpool.go
+++ runtime/workerpool/workerpool.go
@@ -5,23 +5,42 @@ import (
 	"runtime"
 	"sync"
 
+	"github.com/iotaledger/hive.go/core/generics/lo"
 	"github.com/iotaledger/hive.go/core/syncutils"
 )
 
+// WorkerPool is a pool of workers that can execute tasks.
 type WorkerPool struct {
-	Name                string
-	PendingTasksCounter *syncutils.Counter
-	Queue               *syncutils.Stack[*Task]
-	ShutdownComplete    sync.WaitGroup
+	// Name is the name of the WorkerPool.
+	Name string
 
-	workerCount    int
-	isRunning      bool
+	// PendingTasksCounter is the number of tasks that are currently pending.
+	PendingTasksCounter *syncutils.Counter
+
+	// Queue is the queue of tasks that are waiting to be executed.
+	Queue *syncutils.Stack[*Task]
+
+	// ShutdownComplete is a WaitGroup that is used to wait for the WorkerPool to shutdown.
+	ShutdownComplete sync.WaitGroup
+
+	// workerCount is the number of workers that are used to execute tasks.
+	workerCount int
+
+	// isRunning indicates if the WorkerPool is running.
+	isRunning bool
+
+	// dispatcherChan is the channel that is used to dispatch tasks to the workers.
 	dispatcherChan chan *Task
+
+	// shutdownSignal is the channel that is used to signal the workers to shut down.
 	shutdownSignal chan bool
-	mutex          syncutils.RWMutex
+
+	// mutex is used to synchronize access to the WorkerPool.
+	mutex syncutils.RWMutex
 }
 
-func New(name string, optsWorkerCount ...int) (newWorkerPool *WorkerPool) {
+// New creates a new WorkerPool with the given name and returns it.
+func New(name string, optsWorkerCount ...int) *WorkerPool {
 	if len(optsWorkerCount) == 0 {
 		optsWorkerCount = append(optsWorkerCount, 2*runtime.NumCPU())
 	}
@@ -35,7 +54,8 @@ func New(name string, optsWorkerCount ...int) (newWorkerPool *WorkerPool) {
 	}
 }
 
-func (u *WorkerPool) Start() (self *WorkerPool) {
+// Start starts the WorkerPool.
+func (u *WorkerPool) Start() *WorkerPool {
 	u.mutex.Lock()
 	defer u.mutex.Unlock()
 
@@ -51,19 +71,32 @@ func (u *WorkerPool) Start() (self *WorkerPool) {
 	return u
 }
 
-func (u *WorkerPool) Submit(task func(), optStackTrace ...string) {
+// Submit submits a new task to the WorkerPool.
+func (u *WorkerPool) Submit(workerFunc func(), optStackTrace ...string) {
 	if !u.IsRunning() {
 		panic(fmt.Sprintf("worker pool '%s' is not running", u.Name))
 	}
 
-	if u.PendingTasksCounter.Increase(); len(optStackTrace) >= 1 {
-		u.Queue.Push(newTask(func() { u.PendingTasksCounter.Decrease() }, task, optStackTrace[0]))
-	} else {
-		u.Queue.Push(newTask(func() { u.PendingTasksCounter.Decrease() }, task, ""))
-	}
+	u.increasePendingTasks()
+
+	u.Queue.Push(newTask(workerFunc, u.decreasePendingTasks, lo.First(optStackTrace)))
 }
 
-func (u *WorkerPool) Shutdown(cancelPendingTasks ...bool) (self *WorkerPool) {
+// IsRunning returns true if the WorkerPool is running.
+func (u *WorkerPool) IsRunning() bool {
+	u.mutex.RLock()
+	defer u.mutex.RUnlock()
+
+	return u.isRunning
+}
+
+// WorkerCount returns the number of workers that are used to execute tasks.
+func (u *WorkerPool) WorkerCount() int {
+	return u.workerCount
+}
+
+// Shutdown shuts down the WorkerPool.
+func (u *WorkerPool) Shutdown(cancelPendingTasks ...bool) *WorkerPool {
 	u.mutex.Lock()
 	defer u.mutex.Unlock()
 
@@ -80,23 +113,24 @@ func (u *WorkerPool) Shutdown(cancelPendingTasks ...bool) (self *WorkerPool) {
 	return u
 }
 
-func (u *WorkerPool) IsRunning() (isRunning bool) {
-	u.mutex.RLock()
-	defer u.mutex.RUnlock()
-
-	return u.isRunning
+// increasePendingTasks increases the number of pending tasks.
+func (u *WorkerPool) increasePendingTasks() {
+	u.PendingTasksCounter.Increase()
 }
 
-func (u *WorkerPool) WorkerCount() (workerCount int) {
-	return u.workerCount
+// decreasePendingTasks decreases the number of pending tasks.
+func (u *WorkerPool) decreasePendingTasks() {
+	u.PendingTasksCounter.Decrease()
 }
 
+// startDispatcher starts the dispatcher that dispatches tasks to the workers.
 func (u *WorkerPool) startDispatcher() {
 	u.dispatcherChan = make(chan *Task, u.workerCount)
 
 	go u.dispatcher()
 }
 
+// dispatcher is the dispatcher that dispatches tasks to the workers.
 func (u *WorkerPool) dispatcher() {
 	for u.IsRunning() || u.Queue.Size() > 0 {
 		if task, success := u.Queue.PopOrWait(u.IsRunning); success {
@@ -109,6 +143,7 @@ func (u *WorkerPool) dispatcher() {
 	close(u.dispatcherChan)
 }
 
+// startWorkers starts the workers that execute tasks.
 func (u *WorkerPool) startWorkers() {
 	for i := 0; i < u.workerCount; i++ {
 		u.ShutdownComplete.Add(1)
@@ -117,24 +152,26 @@ func (u *WorkerPool) startWorkers() {
 	}
 }
 
+// worker is a worker that executes tasks.
 func (u *WorkerPool) worker() {
 	defer u.ShutdownComplete.Done()
 
 	u.handleShutdown(u.workerReadLoop())
 }
 
-func (u *WorkerPool) workerReadLoop() (shutdownSignal bool) {
+// workerReadLoop reads tasks from the dispatcherChan and executes them.
+func (u *WorkerPool) workerReadLoop() bool {
 	for {
 		select {
-		case shutdownSignal = <-u.shutdownSignal:
-			return
+		case shutdownSignal := <-u.shutdownSignal:
+			return shutdownSignal
 		default:
 			select {
-			case shutdownSignal = <-u.shutdownSignal:
-				return
+			case shutdownSignal := <-u.shutdownSignal:
+				return shutdownSignal
 			case element, success := <-u.dispatcherChan:
 				if !success {
-					return
+					return false
 				}
 
 				element.run()
@@ -143,6 +180,7 @@ func (u *WorkerPool) workerReadLoop() (shutdownSignal bool) {
 	}
 }
 
+// handleShutdown handles the shutdown of the worker.
 func (u *WorkerPool) handleShutdown(cancelPendingTasks bool) {
 	for task, success := <-u.dispatcherChan; success; task, success = <-u.dispatcherChan {
 		if cancelPendingTasks {
