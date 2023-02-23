8caba55e57d4e3e848802cde8e43a7424a3d779f
jonastheis <4181434+jonastheis@users.noreply.github.com>
2023-02-22 18:18:51 -0600

    Add ring buffer

---
+++ ds/ringbuffer/ringbuffer.go
@@ -0,0 +1,56 @@
+package ringbuffer
+
+import (
+	"sync"
+)
+
+// RingBuffer is a thread-safe fixed buffer of elements with FIFO semantics.
+// When the buffer is full, adding a new element overwrites the oldest element.
+type RingBuffer[T any] struct {
+	buffer   []T
+	pos      int
+	capacity int
+	size     int
+	mutex    sync.RWMutex
+}
+
+// NewRingBuffer creates a new RingBuffer with a maximum size of capacity.
+func NewRingBuffer[T any](capacity int) *RingBuffer[T] {
+	return &RingBuffer[T]{
+		buffer:   make([]T, capacity),
+		capacity: capacity,
+	}
+}
+
+// Add adds an element to the buffer, overwriting the oldest element if the buffer is full.
+func (r *RingBuffer[T]) Add(element T) {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
+	r.buffer[r.pos] = element
+	r.pos = (r.pos + 1) % r.capacity
+
+	if r.size < r.capacity {
+		r.size = r.size + 1
+	}
+}
+
+// Get returns all the elements currently in the buffer, from newest to oldest.
+func (r *RingBuffer[T]) Get() []T {
+	r.mutex.RLock()
+	defer r.mutex.RUnlock()
+
+	result := make([]T, r.size)
+	i := r.pos - 1
+	if i < 0 {
+		i = r.capacity - 1
+	}
+	for j := 0; j < r.size; j++ {
+		result[j] = r.buffer[i]
+		i--
+		if i < 0 {
+			i = r.capacity - 1
+		}
+	}
+	return result
+}
---
+++ ds/ringbuffer/ringbuffer_test.go
@@ -0,0 +1,44 @@
+package ringbuffer
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestRingBuffer(t *testing.T) {
+	r := NewRingBuffer[int](3)
+
+	require.Equal(t, []int{}, r.Get())
+
+	r.Add(1)
+	r.Add(2)
+	expected := []int{2, 1}
+	actual := r.Get()
+	require.Len(t, actual, 2)
+	require.Equal(t, expected, actual, "Buffer should contain %v but got %v", expected, actual)
+
+	r.Add(3)
+
+	// Test that the buffer contains the expected elements
+	expected = []int{3, 2, 1}
+	actual = r.Get()
+	require.Equal(t, expected, actual, "Buffer should contain %v but got %v", expected, actual)
+
+	// Add 2 more elements to the buffer, overwriting the oldest element
+	r.Add(4)
+	r.Add(5)
+
+	// Test that the buffer contains the expected elements
+	expected = []int{5, 4, 3}
+	actual = r.Get()
+	require.Equal(t, expected, actual, "Buffer should contain %v but got %v", expected, actual)
+
+	// Add 1 more element to the buffer, overwriting the oldest element
+	r.Add(6)
+
+	// Test that the buffer contains the expected elements
+	expected = []int{6, 5, 4}
+	actual = r.Get()
+	require.Equal(t, expected, actual, "Buffer should contain %v but got %v", expected, actual)
+}
