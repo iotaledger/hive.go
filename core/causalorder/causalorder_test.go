ad6fc7aeeb8949270a6b70c849079803414d7121
jkrvivian <jkrvivian@gmail.com>
2023-05-11 16:48:56 +0800

    Add parallel unit tests in causalorder

--- core/causalorder/causalorder_test.go
+++ core/causalorder/causalorder_test.go
@@ -1,6 +1,7 @@
 package causalorder
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -128,4 +129,86 @@ func TestCausalOrder_UnexpectedCases(t *testing.T) {
 		tf.AssertEvicted("B")
 	}()
 	tf.EvictIndex(1)
+}
+
+func TestCausalOrder_QueueParallel(t *testing.T) {
+	workers := workerpool.NewGroup(t.Name())
+	tf := NewTestFramework(t, workers)
+	var wg sync.WaitGroup
+
+	aliases := map[string]bool{
+		"A": true,
+		"B": true,
+		"C": false,
+		"D": true,
+		"E": true,
+	}
+
+	tf.CreateEntity("A", WithParents(tf.EntityIDs("Genesis")), WithIndex(1))
+	tf.CreateEntity("B", WithParents(tf.EntityIDs("A")), WithIndex(1))
+	tf.CreateEntity("C", WithParents(tf.EntityIDs("A", "B")), WithIndex(1))
+	tf.CreateEntity("D", WithParents(tf.EntityIDs("C", "B")), WithIndex(1))
+	tf.CreateEntity("E", WithParents(tf.EntityIDs("D")), WithIndex(2))
+
+	for alias, queue := range aliases {
+		wg.Add(1)
+		go func(alias string, queue bool) {
+			if queue {
+				tf.Queue(tf.Entity(alias))
+			}
+			wg.Done()
+		}(alias, queue)
+	}
+
+	wg.Wait()
+	workers.WaitChildren()
+	tf.EvictIndex(1)
+	tf.AssertOrdered("A", "B")
+	tf.AssertEvicted("C", "D")
+
+	tf.EvictUntil(2)
+	tf.AssertOrdered("A", "B")
+	tf.AssertEvicted("C", "D", "E")
+}
+
+func TestCausalOrder_EvictParallel(t *testing.T) {
+	workers := workerpool.NewGroup(t.Name())
+	tf := NewTestFramework(t, workers)
+	var wg sync.WaitGroup
+
+	tf.CreateEntity("A", WithParents(tf.EntityIDs("Genesis")), WithIndex(1))
+	tf.CreateEntity("B", WithParents(tf.EntityIDs("A")), WithIndex(1))
+	tf.CreateEntity("C", WithParents(tf.EntityIDs("A", "B")), WithIndex(1))
+	tf.CreateEntity("D", WithParents(tf.EntityIDs("C", "B")), WithIndex(1))
+	tf.CreateEntity("E", WithParents(tf.EntityIDs("D")), WithIndex(2))
+
+	wg.Wait()
+	tf.Queue(tf.Entity("A"))
+	workers.WaitChildren()
+	tf.AssertOrdered("A")
+
+	tf.Queue(tf.Entity("D"))
+	workers.WaitChildren()
+	tf.AssertOrdered("A")
+
+	tf.Queue(tf.Entity("E"))
+	workers.WaitChildren()
+	tf.AssertOrdered("A")
+
+	tf.Queue(tf.Entity("B"))
+	workers.WaitChildren()
+	tf.AssertOrdered("A", "B")
+	tf.AssertEvicted()
+
+	// tf.EvictUntil(2)
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(tf *TestFramework, index uint64) {
+			tf.EvictUntil(index)
+			wg.Done()
+		}(tf, uint64(i))
+	}
+	wg.Wait()
+
+	tf.AssertEvicted("C", "D", "E")
 }
