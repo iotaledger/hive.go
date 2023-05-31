97be4b1d3a360d115aed3330545185e78605b913
Daria Dziubałtowska <daria305@gmail.com>
2023-05-31 12:58:18 +0200

    Add fromBytes and Bytes methods for Empty

--- ds/types/empty.go
+++ ds/types/empty.go
@@ -3,3 +3,11 @@ package types
 type Empty struct{}
 
 var Void = Empty{}
+
+func (e Empty) Bytes() ([]byte, error) {
+	return []byte{}, nil
+}
+
+func (e *Empty) FromBytes([]byte) (int, error) {
+	return 0, nil
+}
