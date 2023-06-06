4201b6bcb0991504ced51bc065124f73711b420b
jkrvivian <jkrvivian@gmail.com>
2023-06-06 22:26:23 +0800

    Add more tests to eventticker

--- core/eventticker/eventticker_test.go
+++ core/eventticker/eventticker_test.go
@@ -2,38 +2,61 @@ package eventticker_test
 
 import (
 	"encoding/binary"
+	"sync"
 	"testing"
 	"time"
 
 	"github.com/iotaledger/hive.go/core/eventticker"
+	"github.com/iotaledger/hive.go/lo"
+	"github.com/iotaledger/iota.go/v4/tpkg"
 	"github.com/mr-tron/base58"
 	"github.com/stretchr/testify/require"
 )
 
 func TestEventTicker(t *testing.T) {
-	eventTicker := eventticker.New[index, testID]()
+	eventTicker := eventticker.New(
+		eventticker.RetryInterval[index, testID](time.Duration(time.Second)),
+		eventticker.RetryJitter[index, testID](time.Duration(time.Millisecond)),
+	)
 
-	id := newtestID(index(1), 0)
+	// Add ticker for slot 1
+	id := newtestID(index(1), tpkg.RandByte())
 	eventTicker.StartTicker(id)
 
 	has := eventTicker.HasTicker(id)
 	require.True(t, has, "EventTicker.HasTicker() returned false, expected true")
 	require.Equalf(t, 1, eventTicker.QueueSize(), "EventTicker.QueueSize() returned %d, expected 1", eventTicker.QueueSize())
 
+	eventTicker.StartTicker(id)
+	require.Equal(t, 1, eventTicker.QueueSize())
+
+	// Stop and delete ticker for slot 1
 	eventTicker.StopTicker(id)
 	has = eventTicker.HasTicker(id)
 	require.False(t, has, "EventTicker.HasTicker() returned true, expected false")
 	require.Equalf(t, 0, eventTicker.QueueSize(), "EventTicker.QueueSize() returned %d, expected 0", eventTicker.QueueSize())
 
-	id1 := newtestID(index(1), 1)
-	id2 := newtestID(index(2), 2)
+	// Add new tickers for slot 1 and 2
+	id1 := newtestID(index(1), tpkg.RandByte())
+	id2 := newtestID(index(2), tpkg.RandByte())
 	eventTicker.StartTickers([]testID{id1, id2})
 
+	require.Equalf(t, 2, eventTicker.QueueSize(), "EventTicker.QueueSize() returned %d, expected 2", eventTicker.QueueSize())
 	has = eventTicker.HasTicker(id1)
 	require.True(t, has, "EventTicker.HasTicker() returned false, expected true")
 	has = eventTicker.HasTicker(id2)
 	require.True(t, has, "EventTicker.HasTicker() returned false, expected true")
 
+	// Evict slot 1
+	eventTicker.EvictUntil(index(1))
+	require.Equalf(t, 1, eventTicker.QueueSize(), "EventTicker.QueueSize() returned %d, expected 1", eventTicker.QueueSize())
+
+	// Add ticker for evicted slot
+	eventTicker.StartTicker(id1)
+	has = eventTicker.HasTicker(id1)
+	require.False(t, has, "EventTicker.HasTicker() returned true, expected false")
+
+	// evict evicted slot
 	eventTicker.EvictUntil(index(1))
 	require.Equalf(t, 1, eventTicker.QueueSize(), "EventTicker.QueueSize() returned %d, expected 1", eventTicker.QueueSize())
 
@@ -51,12 +74,140 @@ func TestEventTicker(t *testing.T) {
 	}
 }
 
+func TestRescheduleTicker(t *testing.T) {
+	eventTicker := eventticker.New(
+		eventticker.RetryInterval[index, testID](time.Duration(time.Second)),
+		eventticker.RetryJitter[index, testID](time.Duration(time.Millisecond)),
+		eventticker.MaxRequestThreshold[index, testID](3),
+	)
+	var mutex sync.Mutex
+	var startTime time.Time
+	counter := 0
+	done := make(chan struct{})
+	id := newtestID(index(1), tpkg.RandByte())
+
+	unhook := lo.Batch(
+		eventTicker.Events.TickerStarted.Hook(func(ti testID) {
+			mutex.Lock()
+			defer mutex.Unlock()
+
+			startTime = time.Now()
+		}).Unhook,
+		eventTicker.Events.Tick.Hook(func(ti testID) {
+			mutex.Lock()
+			defer mutex.Unlock()
+
+			if counter > 0 {
+				require.LessOrEqual(t, time.Since(startTime), time.Duration(1*time.Second+5*time.Millisecond))
+				startTime = time.Now()
+			}
+			counter++
+		}).Unhook,
+		eventTicker.Events.TickerFailed.Hook(func(ti testID) {
+			require.Equal(t, 5, counter)
+			close(done)
+		}).Unhook,
+	)
+
+	eventTicker.StartTicker(id)
+
+	has := eventTicker.HasTicker(id)
+	require.True(t, has, "EventTicker.HasTicker() returned false, expected true")
+
+	select {
+	case <-done:
+		unhook()
+		eventTicker.Shutdown()
+	case <-time.After(10 * time.Second):
+		t.Errorf("Shutdown timed out")
+	}
+}
+
+func TestRescheduleMultipleTicker(t *testing.T) {
+	eventTicker := eventticker.New(
+		eventticker.RetryInterval[index, testID](time.Duration(2*time.Second)),
+		eventticker.RetryJitter[index, testID](time.Duration(time.Millisecond)),
+		eventticker.MaxRequestThreshold[index, testID](2),
+	)
+
+	type info struct {
+		startTime time.Time
+		counter   int
+	}
+
+	tickers := make(map[testID]*info)
+	for i := 1; i <= 5; i++ {
+		id := newtestID(index(i), tpkg.RandByte())
+		tickers[id] = &info{}
+	}
+
+	var mutex sync.Mutex
+	var wg sync.WaitGroup
+
+	wg.Add(len(tickers))
+	unhook := lo.Batch(
+		eventTicker.Events.TickerStarted.Hook(func(ti testID) {
+			mutex.Lock()
+			defer mutex.Unlock()
+
+			t.Logf("ticker %v started", ti)
+			tickers[ti].startTime = time.Now()
+		}).Unhook,
+		eventTicker.Events.Tick.Hook(func(ti testID) {
+			mutex.Lock()
+			defer mutex.Unlock()
+
+			t.Logf("ticker %v ticks", ti)
+			if tickers[ti].counter > 0 {
+				require.LessOrEqual(t, time.Since(tickers[ti].startTime), time.Duration(3*time.Second))
+				tickers[ti].startTime = time.Now()
+			}
+			tickers[ti].counter++
+		}).Unhook,
+		eventTicker.Events.TickerFailed.Hook(func(ti testID) {
+			t.Logf("ticker %v terminated", ti)
+			require.Equal(t, 4, tickers[ti].counter)
+			wg.Done()
+		}).Unhook,
+	)
+
+	issuerIDs := lo.Keys(tickers)
+	require.Equal(t, len(tickers), len(issuerIDs))
+	eventTicker.StartTickers(issuerIDs)
+
+	wg.Wait()
+	unhook()
+	eventTicker.Shutdown()
+}
+
+func TestNonExistedTicker(t *testing.T) {
+	eventTicker := eventticker.New(
+		eventticker.RetryInterval[index, testID](time.Duration(time.Second)),
+		eventticker.RetryJitter[index, testID](time.Duration(time.Millisecond)),
+	)
+
+	id2 := newtestID(index(2), tpkg.RandByte())
+	eventTicker.StartTicker(id2)
+
+	// Test HasTicker with non-existed ticker ID
+	id3 := newtestID(index(2), 3)
+	has := eventTicker.HasTicker(id3)
+	require.False(t, has, "EventTicker.HasTicker() returned true, expected false")
+	eventTicker.StopTicker(id3)
+
+	// Test StopTicker with non-existed slot index
+	id4 := newtestID(index(3), 4)
+	has = eventTicker.HasTicker(id4)
+	require.False(t, has, "EventTicker.HasTicker() returned true, expected false")
+	eventTicker.StopTicker(id4)
+}
+
 type testID [65]byte
 
 func newtestID(i index, idByte byte) testID {
 	id := testID{}
 	copy(id[:], []byte{idByte})
-	binary.LittleEndian.PutUint64(id[0:], uint64(i))
+	binary.LittleEndian.PutUint64(id[1:], uint64(i))
 
 	return id
 }
@@ -71,7 +222,7 @@ func (t *testID) FromBytes(b []byte) (int, error) {
 }
 
 func (t testID) Index() index {
-	return index(binary.LittleEndian.Uint64(t[:63]))
+	return index(binary.LittleEndian.Uint64(t[1:]))
 }
 
 func (t testID) String() string {
