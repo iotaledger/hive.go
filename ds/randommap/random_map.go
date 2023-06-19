be56e1c21d03721100ccb062081fb5e95768cc54
Hans Moog <3293976+hmoog@users.noreply.github.com>
2023-06-19 13:03:31 +0200

    Feat: Add randommap.Values() (#516)

--- ds/randommap/random_map.go
+++ ds/randommap/random_map.go
@@ -30,29 +30,29 @@ func New[K comparable, V any](opts ...shrinkingmap.Option) *RandomMap[K, V] {
 
 // Set associates the specified value with the specified key.
 // If the association already exists, it updates the value.
-func (rmap *RandomMap[K, V]) Set(key K, value V) {
-	rmap.mutex.Lock()
-	defer rmap.mutex.Unlock()
+func (r *RandomMap[K, V]) Set(key K, value V) {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
 
-	if entry, exists := rmap.rawMap.Get(key); exists {
+	if entry, exists := r.rawMap.Get(key); exists {
 		entry.value = value
 	} else {
-		rmap.rawMap.Set(key, &randomMapEntry[K, V]{
+		r.rawMap.Set(key, &randomMapEntry[K, V]{
 			key:      key,
 			value:    value,
-			keyIndex: rmap.rawMap.Size(),
+			keyIndex: r.rawMap.Size(),
 		})
 
-		rmap.keys = append(rmap.keys, key)
+		r.keys = append(r.keys, key)
 	}
 }
 
 // Get returns the value to which the specified key is mapped.
-func (rmap *RandomMap[K, V]) Get(key K) (result V, exists bool) {
-	rmap.mutex.RLock()
-	defer rmap.mutex.RUnlock()
+func (r *RandomMap[K, V]) Get(key K) (result V, exists bool) {
+	r.mutex.RLock()
+	defer r.mutex.RUnlock()
 
-	if entry, entryExists := rmap.rawMap.Get(key); entryExists {
+	if entry, entryExists := r.rawMap.Get(key); entryExists {
 		result = entry.value
 		exists = entryExists
 	}
@@ -61,81 +61,81 @@ func (rmap *RandomMap[K, V]) Get(key K) (result V, exists bool) {
 }
 
 // Has returns a boolean value indicating whether it exists in the map.
-func (rmap *RandomMap[K, V]) Has(key K) bool {
-	rmap.mutex.RLock()
-	defer rmap.mutex.RUnlock()
+func (r *RandomMap[K, V]) Has(key K) bool {
+	r.mutex.RLock()
+	defer r.mutex.RUnlock()
 
-	return rmap.rawMap.Has(key)
+	return r.rawMap.Has(key)
 }
 
 // Delete removes the mapping for the specified key in the map.
-func (rmap *RandomMap[K, V]) Delete(key K) (value V, deleted bool) {
-	rmap.mutex.Lock()
-	defer rmap.mutex.Unlock()
+func (r *RandomMap[K, V]) Delete(key K) (value V, deleted bool) {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
 
-	if entry, exists := rmap.rawMap.Get(key); exists {
-		if entry.keyIndex != len(rmap.keys) {
+	if entry, exists := r.rawMap.Get(key); exists {
+		if entry.keyIndex != len(r.keys) {
 			// move the last key to the position of the deleted key to shrink the slice
 			oldKeyIndex := entry.keyIndex
-			movedKeyIndex := len(rmap.keys) - 1
+			movedKeyIndex := len(r.keys) - 1
 
-			movedKey := rmap.keys[movedKeyIndex]
+			movedKey := r.keys[movedKeyIndex]
 
-			movedEntry, _ := rmap.rawMap.Get(movedKey)
+			movedEntry, _ := r.rawMap.Get(movedKey)
 			movedEntry.keyIndex = oldKeyIndex
 
-			rmap.keys[oldKeyIndex] = movedKey
+			r.keys[oldKeyIndex] = movedKey
 
 			var defaultKey K
-			rmap.keys[movedKeyIndex] = defaultKey
+			r.keys[movedKeyIndex] = defaultKey
 		}
 
-		rmap.keys = rmap.keys[:len(rmap.keys)-1]
+		r.keys = r.keys[:len(r.keys)-1]
 
-		return entry.value, rmap.rawMap.Delete(key)
+		return entry.value, r.rawMap.Delete(key)
 	}
 
 	return
 }
 
 // Size returns the number of key-value mappings in the map.
-func (rmap *RandomMap[K, V]) Size() int {
-	rmap.mutex.RLock()
-	defer rmap.mutex.RUnlock()
+func (r *RandomMap[K, V]) Size() int {
+	r.mutex.RLock()
+	defer r.mutex.RUnlock()
 
-	return rmap.rawMap.Size()
+	return r.rawMap.Size()
 }
 
 // ForEach iterates through the elements in the map and calls the consumer function for each element.
-func (rmap *RandomMap[K, V]) ForEach(consumer func(key K, value V) bool) {
-	rmap.mutex.RLock()
-	defer rmap.mutex.RUnlock()
+func (r *RandomMap[K, V]) ForEach(consumer func(key K, value V) bool) {
+	r.mutex.RLock()
+	defer r.mutex.RUnlock()
 
-	rmap.forEach(consumer)
+	r.forEach(consumer)
 }
 
 // RandomKey returns a random key from the map.
-func (rmap *RandomMap[K, V]) RandomKey() (defaultValue K, exists bool) {
-	rmap.mutex.RLock()
-	defer rmap.mutex.RUnlock()
+func (r *RandomMap[K, V]) RandomKey() (defaultValue K, exists bool) {
+	r.mutex.RLock()
+	defer r.mutex.RUnlock()
 
-	if len(rmap.keys) == 0 {
+	if len(r.keys) == 0 {
 		return defaultValue, false
 	}
 
-	return rmap.randomKey(), true
+	return r.randomKey(), true
 }
 
 // RandomEntry returns a random value from the map.
-func (rmap *RandomMap[K, V]) RandomEntry() (defaultValue V, exists bool) {
-	rmap.mutex.RLock()
-	defer rmap.mutex.RUnlock()
+func (r *RandomMap[K, V]) RandomEntry() (defaultValue V, exists bool) {
+	r.mutex.RLock()
+	defer r.mutex.RUnlock()
 
-	if rmap.rawMap.Size() == 0 {
+	if r.rawMap.Size() == 0 {
 		return defaultValue, false
 	}
 
-	if entry, exists := rmap.rawMap.Get(rmap.randomKey()); exists {
+	if entry, exists := r.rawMap.Get(r.randomKey()); exists {
 		return entry.value, true
 	}
 
@@ -144,9 +144,9 @@ func (rmap *RandomMap[K, V]) RandomEntry() (defaultValue V, exists bool) {
 
 // RandomUniqueEntries returns n random and unique values from the map.
 // When count is equal or bigger than the size of the random map, the every entry in the map is returned.
-func (rmap *RandomMap[K, V]) RandomUniqueEntries(count int) (results []V) {
-	rmap.mutex.RLock()
-	defer rmap.mutex.RUnlock()
+func (r *RandomMap[K, V]) RandomUniqueEntries(count int) (results []V) {
+	r.mutex.RLock()
+	defer r.mutex.RUnlock()
 
 	// zero or negative count results in empty result
 	if count < 1 {
@@ -154,9 +154,9 @@ func (rmap *RandomMap[K, V]) RandomUniqueEntries(count int) (results []V) {
 	}
 
 	// can only return as many as there are in the map
-	if rmap.rawMap.Size() <= count {
-		results = make([]V, 0, rmap.rawMap.Size())
-		rmap.forEach(func(key K, value V) bool {
+	if r.rawMap.Size() <= count {
+		results = make([]V, 0, r.rawMap.Size())
+		r.forEach(func(key K, value V) bool {
 			results = append(results, value)
 			return true
 		})
@@ -166,12 +166,12 @@ func (rmap *RandomMap[K, V]) RandomUniqueEntries(count int) (results []V) {
 
 	// helper to keep track of already seen keys
 	results = make([]V, 0, count)
-	randomOrder := rand.Perm(len(rmap.keys))
+	randomOrder := rand.Perm(len(r.keys))
 
 	// there has to be at least (count + 1) key value pairs in the map
 	for idx := 0; idx < len(randomOrder) && len(results) < count; idx++ {
-		randomKey := rmap.keys[randomOrder[idx]]
-		if randomEntry, exists := rmap.rawMap.Get(randomKey); exists {
+		randomKey := r.keys[randomOrder[idx]]
+		if randomEntry, exists := r.rawMap.Get(randomKey); exists {
 			results = append(results, randomEntry.value)
 		}
 	}
@@ -180,25 +180,38 @@ func (rmap *RandomMap[K, V]) RandomUniqueEntries(count int) (results []V) {
 }
 
 // Keys returns the list of keys stored in the RandomMap.
-func (rmap *RandomMap[K, V]) Keys() (result []K) {
-	rmap.mutex.RLock()
-	defer rmap.mutex.RUnlock()
+func (r *RandomMap[K, V]) Keys() (result []K) {
+	r.mutex.RLock()
+	defer r.mutex.RUnlock()
 
-	result = make([]K, rmap.rawMap.Size())
-	copy(result, rmap.keys)
+	result = make([]K, r.rawMap.Size())
+	copy(result, r.keys)
+
+	return
+}
+
+// Values returns the list of values stored in the RandomMap.
+func (r *RandomMap[K, V]) Values() (result []V) {
+	r.mutex.RLock()
+	defer r.mutex.RUnlock()
+
+	r.forEach(func(key K, value V) bool {
+		result = append(result, value)
+		return true
+	})
 
 	return
 }
 
 // randomKey gets a random key from the map.
-func (rmap *RandomMap[K, V]) randomKey() (result K) {
+func (r *RandomMap[K, V]) randomKey() (result K) {
 	//nolint:gosec // we do not care about weak random numbers here
-	return rmap.keys[rand.Intn(rmap.rawMap.Size())]
+	return r.keys[rand.Intn(r.rawMap.Size())]
 }
 
 // forEach executes a function for all key-value pairs in the map.
-func (rmap *RandomMap[K, V]) forEach(consumer func(key K, value V) bool) {
-	rmap.rawMap.ForEach(func(key K, entry *randomMapEntry[K, V]) bool {
+func (r *RandomMap[K, V]) forEach(consumer func(key K, value V) bool) {
+	r.rawMap.ForEach(func(key K, entry *randomMapEntry[K, V]) bool {
 		return consumer(key, entry.value)
 	})
 }
