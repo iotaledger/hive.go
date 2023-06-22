be5857115ec5bb9aa053a15656523cfe51a2244f
Alexander Sporn <github@alexsporn.de>
2023-06-22 20:42:47 +0200

    Cleaned up the SeatedAccounts

--- core/account/seated_accounts.go
+++ core/account/seated_accounts.go
@@ -11,65 +11,53 @@ type SeatIndex int
 type SeatedAccounts[AccountID IDType, AccountIDPtr serializer.MarshalablePtr[AccountID]] struct {
 	accounts       *Accounts[AccountID, AccountIDPtr]
 	seatsByAccount *shrinkingmap.ShrinkingMap[AccountID, SeatIndex]
-	accountsBySeat *shrinkingmap.ShrinkingMap[SeatIndex, AccountID]
 }
 
 func NewSeatedAccounts[A IDType, APtr serializer.MarshalablePtr[A]](accounts *Accounts[A, APtr], optMembers ...A) *SeatedAccounts[A, APtr] {
-	newWeightedSet := new(SeatedAccounts[A, APtr])
-	newWeightedSet.accounts = accounts
-	newWeightedSet.seatsByAccount = shrinkingmap.New[A, SeatIndex]()
-	newWeightedSet.accountsBySeat = shrinkingmap.New[SeatIndex, A]()
-
-	for i, member := range optMembers {
-		newWeightedSet.seatsByAccount.Set(member, SeatIndex(i))
-		newWeightedSet.accountsBySeat.Set(SeatIndex(i), member)
+	s := &SeatedAccounts[A, APtr]{
+		accounts:       accounts,
+		seatsByAccount: shrinkingmap.New[A, SeatIndex](),
 	}
 
-	return newWeightedSet
+	for i, member := range optMembers {
+		s.seatsByAccount.Set(member, SeatIndex(i))
+	}
+
+	return s
 }
 
-func (w *SeatedAccounts[AccountID, AccountIDPtr]) Set(seat SeatIndex, id AccountID) bool {
+func (s *SeatedAccounts[AccountID, AccountIDPtr]) Set(seat SeatIndex, id AccountID) bool {
 	// Check if the account exists.
-	if _, exists := w.accounts.Get(id); !exists {
+	if _, exists := s.accounts.Get(id); !exists {
 		return false
 	}
 
 	// Check if the account already has a seat.
-	if oldSeat, exists := w.seatsByAccount.Get(id); exists {
+	if oldSeat, exists := s.seatsByAccount.Get(id); exists {
 		if oldSeat != seat {
 			return false
 		}
 	}
 
-	w.seatsByAccount.Set(id, seat)
-	w.accountsBySeat.Set(seat, id)
-
-	return true
+	return s.seatsByAccount.Set(id, seat)
 }
 
-func (w *SeatedAccounts[AccountID, AccountIDPtr]) Delete(id AccountID) bool {
-	if oldSeat, exists := w.seatsByAccount.Get(id); exists {
-		w.seatsByAccount.Delete(id)
-		w.accountsBySeat.Delete(oldSeat)
-
-		return true
-	}
-
-	return false
+func (s *SeatedAccounts[AccountID, AccountIDPtr]) Delete(id AccountID) bool {
+	return s.seatsByAccount.Delete(id)
 }
 
-func (w *SeatedAccounts[AccountID, AccountIDPtr]) GetSeat(id AccountID) (seat SeatIndex, exists bool) {
-	return w.seatsByAccount.Get(id)
+func (s *SeatedAccounts[AccountID, AccountIDPtr]) GetSeat(id AccountID) (seat SeatIndex, exists bool) {
+	return s.seatsByAccount.Get(id)
 }
 
-func (w *SeatedAccounts[AccountID, AccountIDPtr]) HasAccount(id AccountID) (has bool) {
-	return w.seatsByAccount.Has(id)
+func (s *SeatedAccounts[AccountID, AccountIDPtr]) HasAccount(id AccountID) (has bool) {
+	return s.seatsByAccount.Has(id)
 }
 
-func (w *SeatedAccounts[AccountID, AccountIDPtr]) SeatCount() int {
-	return w.accountsBySeat.Size()
+func (s *SeatedAccounts[AccountID, AccountIDPtr]) SeatCount() int {
+	return s.seatsByAccount.Size()
 }
 
-func (w *SeatedAccounts[AccountID, AccountIDPtr]) Accounts() *advancedset.AdvancedSet[AccountID] {
-	return advancedset.New(w.seatsByAccount.Keys()...)
+func (s *SeatedAccounts[AccountID, AccountIDPtr]) Accounts() *advancedset.AdvancedSet[AccountID] {
+	return advancedset.New(s.seatsByAccount.Keys()...)
 }
