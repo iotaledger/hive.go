e29b6562856c39459d493713fb219ec05f63a60b
Andrea V <1577639+karimodm@users.noreply.github.com>
2023-07-11 14:11:41 +0200

    Walk struct pointers recursively with a maxDepth limit

--- runtime/memanalyzer/memanalyzer.go
+++ runtime/memanalyzer/memanalyzer.go
@@ -9,6 +9,8 @@ import (
 	"github.com/fjl/memsize"
 )
 
+const maxDepth = 10
+
 // MemoryReport returns a human-readable report of the memory usage of the given struct pointer, useful to find leaks.
 // Please note that this function "stops the world" when scanning referenced memory from a specific struct field,
 // therefore it can produce significant hiccups when called on big, nested structures.
@@ -21,7 +23,21 @@ func MemoryReport(ptr interface{}) string {
 }
 
 func memoryReport(v reflect.Value, indent int, stringBuilder *strings.Builder) {
+	if indent/2 > maxDepth {
+		return
+	}
+
 	t := v.Type()
+
+	// dereference pointers to structs
+	if t.Kind() == reflect.Pointer && t.Elem().Kind() == reflect.Struct {
+		if v.IsNil() {
+			return
+		}
+		v = v.Elem()
+		t = v.Type()
+	}
+
 	// walk down the fields
 	if t.Kind() == reflect.Struct {
 		for numField := 0; numField < t.NumField(); numField++ {
@@ -33,6 +49,10 @@ func memoryReport(v reflect.Value, indent int, stringBuilder *strings.Builder) {
 				fV = reflect.NewAt(fT.Type, unsafe.Pointer(v.Field(numField).UnsafeAddr()))
 			} else {
 				fV = v.Field(numField)
+			}
+
+			if fV.IsNil() {
+				continue
 			}
 
 			fmt.Fprintf(stringBuilder, "%*s%s %s = %s\n", indent, "", fT.Name, fT.Type, memsize.HumanSize(memsize.Scan(fV.Interface()).Total))
