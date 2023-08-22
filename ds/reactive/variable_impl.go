fda32b111de9b44225db6dc5dea9b6623b6a37e0
Hans Moog <3293976+hmoog@users.noreply.github.com>
2023-08-22 13:44:30 +0200

    Fix: fixed possible reassign of triggered values

--- ds/reactive/variable_impl.go
+++ ds/reactive/variable_impl.go
@@ -157,20 +157,25 @@ func (r *readableVariable[Type]) OnUpdate(callback func(prevValue, newValue Type
 // OnUpdateOnce registers the given callback for the next update and then automatically unsubscribes it. It is possible
 // to provide an optional condition that has to be satisfied for the callback to be triggered.
 func (r *readableVariable[Type]) OnUpdateOnce(callback func(oldValue Type, newValue Type), optCondition ...func(oldValue Type, newValue Type) bool) {
+	callbackTriggered := NewEvent()
 	var triggeredPreValue, triggeredNewValue Type
 
-	triggered := NewEvent()
-
 	unsubscribe := r.OnUpdate(func(prevValue, newValue Type) {
-		if len(optCondition) == 0 || optCondition[0](prevValue, newValue) {
-			triggeredPreValue = prevValue
-			triggeredNewValue = newValue
-
-			triggered.Trigger()
+		if callbackTriggered.Get() {
+			return
 		}
+
+		if len(optCondition) != 0 && !optCondition[0](prevValue, newValue) {
+			return
+		}
+
+		triggeredPreValue = prevValue
+		triggeredNewValue = newValue
+
+		callbackTriggered.Trigger()
 	})
 
-	triggered.OnTrigger(func() {
+	callbackTriggered.OnTrigger(func() {
 		go unsubscribe()
 
 		callback(triggeredPreValue, triggeredNewValue)
