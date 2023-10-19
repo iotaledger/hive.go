f84a26ec79619b938f8d3ffaea862484ce727b46
jonastheis <4181434+jonastheis@users.noreply.github.com>
2023-10-19 12:19:54 +0800

    Fix linter warnings in crypto

--- crypto/ed25519/public_key.go
+++ crypto/ed25519/public_key.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/iotaledger/hive.go/ierrors"
 	"github.com/iotaledger/hive.go/lo"
-	"github.com/iotaledger/hive.go/serializer/v2/marshalutil"
 )
 
 // PublicKey is the type of Ed25519 public keys.
@@ -27,8 +26,11 @@ func PublicKeyFromString(s string) (publicKey PublicKey, err error) {
 
 // PublicKeyFromBytes creates a PublicKey from the given bytes.
 func PublicKeyFromBytes(bytes []byte) (result PublicKey, consumedBytes int, err error) {
-	consumedBytes, err = (&result).FromBytes(bytes)
-	return
+	if len(bytes) < PublicKeySize {
+		return PublicKey{}, 0, ErrNotEnoughBytes
+	}
+
+	return PublicKey(bytes), PublicKeySize, nil
 }
 
 // NativeToPublicKeys converts crypto/ed25519 native public keys into a []PublicKey.
@@ -62,29 +64,9 @@ func RecoverKey(key, data, sig []byte) (result PublicKey, err error) {
 	return
 }
 
-func ParsePublicKey(marshalUtil *marshalutil.MarshalUtil) (PublicKey, error) {
-	id, err := marshalUtil.Parse(func(data []byte) (interface{}, int, error) { return PublicKeyFromBytes(data) })
-	if err != nil {
-		return PublicKey{}, err
-	}
-
-	return id.(PublicKey), nil
-}
-
 // VerifySignature reports whether signature is a valid signature of message by publicKey.
 func (publicKey PublicKey) VerifySignature(data []byte, signature Signature) bool {
 	return Verify(publicKey[:], data, signature[:])
-}
-
-// FromBytes initialized the PublicKey from the given bytes.
-func (publicKey *PublicKey) FromBytes(bytes []byte) (int, error) {
-	if len(bytes) < PublicKeySize {
-		return 0, ErrNotEnoughBytes
-	}
-
-	copy(publicKey[:], bytes)
-
-	return PublicKeySize, nil
 }
 
 // Bytes returns the publicKey in bytes.
@@ -97,32 +79,9 @@ func (publicKey PublicKey) String() string {
 	return base58.Encode(publicKey[:])
 }
 
-func (publicKey *PublicKey) UnmarshalBinary(bytes []byte) error {
-	if _, err := publicKey.FromBytes(bytes); err != nil {
-		return err
-	}
-
-	return nil
-}
-
 // MarshalJSON serializes public key to JSON as base58 encoded string.
 func (publicKey PublicKey) MarshalJSON() ([]byte, error) {
 	return json.Marshal(publicKey.String())
-}
-
-// UnmarshalJSON parses public key from JSON in base58 encoding.
-func (publicKey *PublicKey) UnmarshalJSON(b []byte) error {
-	var s string
-	if err := json.Unmarshal(b, &s); err != nil {
-		return err
-	}
-	pk, err := PublicKeyFromString(s)
-	if err != nil {
-		return ierrors.Wrap(err, "failed to parse public key from JSON")
-	}
-	*publicKey = pk
-
-	return nil
 }
 
 // ToEd25519 returns the public key as native crypto/ed25519.PublicKey.
