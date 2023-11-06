88c1e8ade42cc8d773e2f9dc9d888582bdc2625f
muXxer <git@muxxer.de>
2023-11-06 12:23:11 +0100

    Also return context.Canceled for ErrMergedContextCanceled

--- runtime/contextutils/merged_context.go
+++ runtime/contextutils/merged_context.go
@@ -59,7 +59,7 @@ func MergeContexts(ctxPrimary context.Context, ctxSecondary context.Context) (co
 	go func() {
 		select {
 		case <-mc.cancelCtx.Done():
-			setCtxDoneFunc(ErrMergedContextCanceled)
+			setCtxDoneFunc(ierrors.Join(context.Canceled, ErrMergedContextCanceled))
 		case <-mc.ctxPrimary.Done():
 			setCtxDoneFunc(mc.ctxPrimary.Err())
 		case <-mc.ctxSecondary.Done():
@@ -68,7 +68,7 @@ func MergeContexts(ctxPrimary context.Context, ctxSecondary context.Context) (co
 	}()
 
 	var mergedCancelFunc context.CancelFunc = func() {
-		setCtxDoneFunc(ErrMergedContextCanceled)
+		setCtxDoneFunc(ierrors.Join(context.Canceled, ErrMergedContextCanceled))
 	}
 
 	return mc, mergedCancelFunc
