8b1abc1aa560822e4f5c6dccb86c55d89a247fab
Hans Moog <3293976+hmoog@users.noreply.github.com>
2024-02-23 13:07:41 +0100

    Feat: Added BatchReverse

--- lo/batch.go
+++ lo/batch.go
@@ -1,9 +1,21 @@
 package lo
 
+// Batch creates a new function that calls the given functions in order (nil entries are ignored).
 func Batch(callbacks ...func()) func() {
 	return func() {
 		for _, callback := range callbacks {
 			if callback != nil {
+				callback()
+			}
+		}
+	}
+}
+
+// BatchReverse creates a new function that calls the given functions in reverse order (nil entries are ignored).
+func BatchReverse(callbacks ...func()) func() {
+	return func() {
+		for i := len(callbacks) - 1; i >= 0; i-- {
+			if callback := callbacks[i]; callback != nil {
 				callback()
 			}
 		}
