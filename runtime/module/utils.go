658ef8b73c89b66e1794c4ce3a7a160aaec586b1
Hans Moog <3293976+hmoog@users.noreply.github.com>
2024-03-12 13:55:39 +0100

    Feat: added utility method for easier simple setups

--- runtime/module/utils.go
+++ runtime/module/utils.go
@@ -22,3 +22,24 @@ func WaitAll(event func(Module) reactive.Event, modules ...Module) reactive.Wait
 
 	return wg
 }
+
+// InitSimpleLifecycle is a helper function that sets up the default lifecycle for the given module. If no shutdown
+// function is provided, then the default shutdown function will be used, which automatically triggers the StoppedEvent.
+//
+// Note: If a more complex lifecycle is required, then it needs to be implemented manually.
+func InitSimpleLifecycle[T Module](m T, optShutdown ...func(T)) T {
+	if len(optShutdown) == 0 {
+		m.ShutdownEvent().OnTrigger(func() {
+			m.StoppedEvent().Trigger()
+		})
+	} else {
+		m.ShutdownEvent().OnTrigger(func() {
+			optShutdown[0](m)
+		})
+	}
+
+	m.ConstructedEvent().Trigger()
+	m.InitializedEvent().Trigger()
+
+	return m
+}
